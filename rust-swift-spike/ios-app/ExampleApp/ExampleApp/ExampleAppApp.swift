05258d043879a858a0880b1198a74cb987f1d683
oskarth <oskarth@titanproxy.com>
2023-08-30 13:14:29 +0800

    feat(spike): Init new Xcode iOS example app

---
+++ rust-swift-spike/ios-app/ExampleApp/ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by User Name on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
