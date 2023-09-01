8cdf739f4e42b9e07a32dd640fac3fe840415eb2
oskarth <oskarth@titanproxy.com>
2023-09-01 17:43:46 +0800

    feat(spike): ios-app-2 fresh

---
+++ rust-swift-spike/ios-app-2/FooBar/FooBar/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FooBar
+//
+//  Created by User Name on 9/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rust-swift-spike/ios-app-2/FooBar/FooBar/FooBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FooBarApp.swift
+//  FooBar
+//
+//  Created by User Name on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FooBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
