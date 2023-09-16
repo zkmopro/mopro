b260ee5ff3010884b258963196a4b6437d0e784a
oskarth <oskarth@titanproxy.com>
2023-09-16 13:43:42 +0800

    feat(ios): Init MoproKit CocoaPods

---
+++ mopro-ios/MoproKit/Example/MoproKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MoproKit
+//
+//  Created by 1552237 on 09/16/2023.
+//  Copyright (c) 2023 1552237. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
