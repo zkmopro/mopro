f3a037efa00b9034233f44b447d1e1698c7cf6a0
oskarth <oskarth@titanproxy.com>
2023-09-16 14:01:28 +0800

    feat(ios): Hello world example app

--- mopro-ios/MoproKit/Example/MoproKit/ViewController.swift
+++ mopro-ios/MoproKit/Example/MoproKit/ViewController.swift
@@ -12,7 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Create a UILabel instance
+        let helloLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+
+        // Set the label's properties
+        helloLabel.center = view.center
+        helloLabel.textAlignment = .center
+        helloLabel.text = "Hello, World!"
+
+        // Add the label to the main view
+        view.addSubview(helloLabel)
     }
 
     override func didReceiveMemoryWarning() {
