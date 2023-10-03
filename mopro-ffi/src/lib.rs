c7a56fd13af1110e13da20378a2c9a097cc13b55
oskarth <oskarth@titanproxy.com>
2023-10-03 14:15:33 +0800

    feat(ffi): Update FFI with MoproCircom and Setup

--- mopro-ffi/src/lib.rs
+++ mopro-ffi/src/lib.rs
@@ -1,5 +1,6 @@
-use std::error;
+use std::sync::RwLock;
 
+use ark_serialize::CanonicalSerialize;
 use mopro_core::middleware::circom;
 use mopro_core::MoproError;
 
@@ -11,14 +12,59 @@ pub enum FFIError {
     SerializationError(String),
 }
 
+#[derive(Debug, Clone)]
+pub struct SetupResult {
+    pub provingKey: Vec<u8>,
+    pub inputs: Vec<u8>,
+}
+
 impl From<mopro_core::MoproError> for FFIError {
     fn from(error: mopro_core::MoproError) -> Self {
         FFIError::MoproError(error)
     }
 }
 
-// name of the .udl file
 uniffi::include_scaffolding!("mopro_uniffi");
+
+pub struct MoproCircom {
+    state: RwLock<circom::CircomState>,
+}
+
+// TODO: Use setup, prove and verify functions from mopro_core
+
+// TODO: Use FFIError::SerializationError instead
+impl MoproCircom {
+    pub fn new() -> Self {
+        Self {
+            state: RwLock::new(circom::CircomState::new()),
+        }
+    }
+
+    pub fn setup(&self, wasm_path: String, r1cs_path: String) -> Result<SetupResult, MoproError> {
+        // Lock the state for writing since we're potentially modifying it
+        let mut state_guard = self.state.write().unwrap();
+
+        let (pk, inputs) = state_guard.setup(wasm_path.as_str(), r1cs_path.as_str())?;
+
+        let mut proving_key_bytes = Vec::new();
+        pk.serialize_uncompressed(&mut proving_key_bytes)
+            .map_err(|_| {
+                mopro_core::MoproError::CircomError("Failed to serialize proving key".to_string())
+            })?;
+
+        let mut inputs_bytes = Vec::new();
+        inputs
+            .serialize_uncompressed(&mut inputs_bytes)
+            .map_err(|_| {
+                mopro_core::MoproError::CircomError("Failed to serialize inputs".to_string())
+            })?;
+
+        Ok(SetupResult {
+            provingKey: proving_key_bytes,
+            inputs: inputs_bytes,
+        })
+    }
+}
 
 fn add(a: u32, b: u32) -> u32 {
     a + b
@@ -28,17 +74,10 @@ fn hello() -> String {
     "Hello World from Rust".to_string()
 }
 
-// TODO: Use setup, prove and verify functions from mopro_core
-
-// Result<( ProvingKey<Bn254>, CircomCircuit<Bn254>, ThreadRng, Vec<<Bn254 as Pairing>::ScalarField>,
-fn setup(wasm_path: String, r1cs_path: String) -> Result<(), MoproError> {
-    let res = circom::setup(wasm_path.as_str(), r1cs_path.as_str())?;
-
-    // TODO: Implement FFI-friendly types and return this
-    // Result<( ProvingKey<Bn254>, CircomCircuit<Bn254>, ThreadRng, Vec<<Bn254 as Pairing>::ScalarField>,
-    let (pk, circuit, rng, s) = res;
-
-    Err(MoproError::CircomError("Not implemented yet".to_string()))
+// XXX: Do we need this?
+pub fn init_circom_state() -> Result<(), MoproError> {
+    //let mut circom_state = circom::CircomState::new();
+    Ok(())
 }
 
 // UniFFI expects String type
