8bf207578e8ecef9e73cccad8b4d8b9bf7a9368d
oskarth <oskarth@titanproxy.com>
2023-10-04 14:31:39 +0800

    test(ffi): Add basic Swift test

--- mopro-ffi/tests/bindings/test_mopro.swift
+++ mopro-ffi/tests/bindings/test_mopro.swift
@@ -1,5 +1,16 @@
-//import moproimport MoproKit
 import mopro
 
 let moproCircom = MoproCircom()
-assert(1 == 1)  // Your actual tests here.
+
+let wasmPath = "./../mopro-core/examples/circom/target/multiplier2_js/multiplier2.wasm"
+let r1csPath = "./../mopro-core/examples/circom/target/multiplier2.r1cs"
+
+do {
+    let setupResult = try moproCircom.setup(wasmPath: wasmPath, r1csPath: r1csPath)
+    assert(!setupResult.provingKey.isEmpty, "Proving key should not be empty")
+    assert(!setupResult.inputs.isEmpty, "Inputs should not be empty")
+} catch let error as MoproError {
+    print("MoproError: \(error)")
+} catch {
+    print("Unexpected error: \(error)")
+}
