eb5d5d082b3e397636a794cdc720709f13ff2a7e
oskarth <oskarth@titanproxy.com>
2023-10-09 20:11:49 +0800

    test(ffi): Update Swift e2e tests

--- mopro-ffi/tests/bindings/test_mopro.swift
+++ mopro-ffi/tests/bindings/test_mopro.swift
@@ -9,14 +9,18 @@ do {
     // Setup
     let setupResult = try moproCircom.setup(wasmPath: wasmPath, r1csPath: r1csPath)
     assert(!setupResult.provingKey.isEmpty, "Proving key should not be empty")
-    assert(!setupResult.inputs.isEmpty, "Inputs should not be empty")
+
+    // Prepare inputs
+    var inputs = [String: [Int32]]()
+    inputs["a"] = [3]
+    inputs["b"] = [5]
 
     // Generate Proof
-    let generateProofResult = try moproCircom.generateProof()
+    let generateProofResult = try moproCircom.generateProof(circuitInputs: inputs)
     assert(!generateProofResult.proof.isEmpty, "Proof should not be empty")
 
     // Verify Proof
-    let isValid = try moproCircom.verifyProof(proof: generateProofResult.proof, publicInput: setupResult.inputs)
+    let isValid = try moproCircom.verifyProof(proof: generateProofResult.proof, publicInput: generateProofResult.inputs)
     assert(isValid, "Proof verification should succeed")
 
 } catch let error as MoproError {
