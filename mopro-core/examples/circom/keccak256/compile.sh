e15ead334dd989624504030195fdb50ae314a63d
oskarth <oskarth@titanproxy.com>
2023-10-09 14:36:14 +0800

    refactor(core/circom): keccak256 name

--- mopro-core/examples/circom/keccak256/compile.sh
+++ mopro-core/examples/circom/keccak256/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#circom ./keccak_256_256_test.circom --r1cs --wasm --sym --output ./target
-circom ./examples/circom/keccak256/keccak_256_256_test.circom --r1cs --wasm --sym --output ./examples/circom/keccak256/target
+#circom ./keccak256_256_test.circom --r1cs --wasm --sym --output ./target
+circom ./examples/circom/keccak256/keccak256_256_test.circom --r1cs --wasm --sym --output ./examples/circom/keccak256/target
