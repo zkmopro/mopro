08970547b8db22d9b263b51541a410fe916537b9
oskarth <oskarth@titanproxy.com>
2023-10-25 15:34:43 +0800

    chore(core): Misc build tweaks

--- mopro-core/build.rs
+++ mopro-core/build.rs
@@ -1,35 +1,38 @@
 use color_eyre::eyre::Result;
 
 #[cfg(feature = "dylib")]
-fn build_dylib() -> Result<()> {
-    use color_eyre::eyre::eyre;
-    use enumset::enum_set;
-    use enumset::EnumSet;
+fn build_dylib(wasm_path: &str, dylib_name: &str) -> Result<()> {
     use std::path::Path;
     use std::path::PathBuf;
     use std::{env, fs, str::FromStr};
 
+    use color_eyre::eyre::eyre;
+    use enumset::enum_set;
+    use enumset::EnumSet;
+
     use wasmer::Cranelift;
+    use wasmer::Dylib;
     use wasmer::Target;
     use wasmer::{Module, Store, Triple};
 
-    use wasmer::Dylib;
-
-    let wasm_path = "./../mopro-core/examples/circom/keccak256/target/keccak256_256_test_js/keccak256_256_test.wasm";
-    let wasm_file = Path::new(wasm_path).to_path_buf();
-
-    // TODO: Improve this, we want it to be by architecture
-    // We can copy assets and clean up install_paths after
     let out_dir = env::var("OUT_DIR")?;
+    let project_dir = env::var("CARGO_MANIFEST_DIR")?;
+    let build_mode = env::var("PROFILE")?;
+    let target_arch = env::var("TARGET")?;
+
     let out_dir = Path::new(&out_dir).to_path_buf();
-    let dylib_file = out_dir.join("keccak256.dylib");
+    let wasm_file = Path::new(wasm_path).to_path_buf();
+    let dylib_file = out_dir.join(dylib_name);
+    let final_dir = PathBuf::from(&project_dir)
+        .join("target")
+        .join(build_mode)
+        .join(&target_arch);
 
     // if dylib_file.exists() {
     //     return Ok(());
     // }
 
     // Create a WASM engine for the target that can compile
-    let target_arch = env::var("TARGET")?;
     let triple = Triple::from_str(&target_arch).map_err(|e| eyre!(e))?;
     let cpu_features = enum_set!();
     let target = Target::new(triple, cpu_features);
@@ -43,16 +46,8 @@ fn build_dylib() -> Result<()> {
     assert!(dylib_file.exists());
 
     // Copy dylib to a more predictable path
-    let target_arch = env::var("TARGET")?;
-    let project_dir = env::var("CARGO_MANIFEST_DIR")?;
-    let final_dir = PathBuf::from(&project_dir)
-        .join("target/debug")
-        .join(target_arch);
-
-    // Ensure the final directory exists
     fs::create_dir_all(&final_dir)?;
-
-    let final_path = final_dir.join("keccak256.dylib");
+    let final_path = final_dir.join(dylib_name);
     fs::copy(&dylib_file, &final_path)?;
     println!("cargo:warning=Dylib location: {}", final_path.display());
 
@@ -61,6 +56,10 @@ fn build_dylib() -> Result<()> {
 
 fn main() -> Result<()> {
     #[cfg(feature = "dylib")]
-    build_dylib()?;
+    {
+        let wasm_path = "./../mopro-core/examples/circom/keccak256/target/keccak256_256_test_js/keccak256_256_test.wasm";
+        let dylib_name = "keccak256.dylib";
+        build_dylib(wasm_path, dylib_name)?;
+    }
     Ok(())
 }
