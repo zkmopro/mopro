9df2e94b468b6639786f83be43ab3b701e2f1deb
oskarth <oskarth@titanproxy.com>
2023-10-26 19:27:45 +0800

    build(core): Improve circom prepare and compile scripts

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,13 +4,27 @@
 set -euo pipefail
 
 PROJECT_DIR=$(pwd)
+CIRCOM_DIR="${PROJECT_DIR}/mopro-core/examples/circom"
 
-# build circom circuits in mopro-core
-cd ${PROJECT_DIR}/mopro-core/examples/circom/keccak256
-npm install
-./compile.sh
+# Build Circom circuits in mopro-core and run trusted setup
+echo "mopro-core: Compiling example circuits..."
+cd $CIRCOM_DIR
 
-# build ffi in mopro-ffi
+# NOTE: multiplier2 is already pre-compiled
+# echo "Compiling multiplier example circuit..."
+# ./scripts/compile.sh multiplier2 multiplier2.circom
+
+# Setup and compile keccak256
+echo "Compiling keccak256 example circuit..."
+(cd keccak256 && npm install)
+./scripts/compile.sh keccak256 keccak256_256_test.circom
+
+# TODO: Finish trusted setup script
+#echo "mopro-core: Running trusted setup for keccak256..."
+#./scripts/trusted_setup.sh
+
+# Add support for target architectures
+echo "mopro-ffi: Adding support for target architectures..."
 cd ${PROJECT_DIR}/mopro-ffi
 rustup target add x86_64-apple-ios aarch64-apple-ios aarch64-apple-ios-sim
 
