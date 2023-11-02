c6c59fea30341daabe937432afb093d25dd52884
oskarth <oskarth@titanproxy.com>
2023-11-02 14:13:48 +0800

    test(ffi): Add generate_proof2 zkey test

--- mopro-ffi/tests/test_generated_bindings.rs
+++ mopro-ffi/tests/test_generated_bindings.rs
@@ -4,4 +4,5 @@ uniffi::build_foreign_language_testcases!(
     //    "tests/bindings/test_mopro.rb",
     //    "tests/bindings/test_mopro.py",
     "tests/bindings/test_mopro_keccak.swift",
+    "tests/bindings/test_mopro_keccak2.swift",
 );
