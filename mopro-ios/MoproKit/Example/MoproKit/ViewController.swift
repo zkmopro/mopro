bffedf6df4ec8ead382c33b1697b43fa15e8232b
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-11-03 17:13:03 +0800

    test(ios): add circom and ui tests

--- mopro-ios/MoproKit/Example/MoproKit/ViewController.swift
+++ mopro-ios/MoproKit/Example/MoproKit/ViewController.swift
@@ -94,23 +94,12 @@ class ViewController: UIViewController {
             var inputs = [String: [String]]()
             inputs["in"] = bits
 
-            // Expected outputs
-            let outputVec: [UInt8] = [
-                37, 17, 98, 135, 161, 178, 88, 97, 125, 150, 143, 65, 228, 211, 170, 133, 153, 9, 88,
-                212, 4, 212, 175, 238, 249, 210, 214, 116, 170, 85, 45, 21,
-            ]
-            let outputBits: [String] = bytesToBits(bytes: outputVec)
-            let expectedOutput: [UInt8] = serializeOutputs(outputBits)
-
             // Multiplier example
             // var inputs = [String: [String]]()
             // let a = 3
             // let b = 5
-            // let c = a*b
             // inputs["a"] = [String(a)]
             // inputs["b"] = [String(b)]
-            // let outputs: [String] = [String(c), String(a)]
-            // let expectedOutput: [UInt8] = serializeOutputs(outputs)
 
             // Record start time
             let start = CFAbsoluteTimeGetCurrent()
@@ -118,7 +107,6 @@ class ViewController: UIViewController {
             // Generate Proof
             let generateProofResult = try moproCircom.generateProof(circuitInputs: inputs)
             assert(!generateProofResult.proof.isEmpty, "Proof should not be empty")
-            assert(Data(expectedOutput) == generateProofResult.inputs, "Circuit outputs mismatch the expected outputs")
 
             // Record end time and compute duration
             let end = CFAbsoluteTimeGetCurrent()
@@ -173,29 +161,4 @@ func bytesToBits(bytes: [UInt8]) -> [String] {
         }
     }
     return bits
-}
-
-func serializeOutputs(_ stringArray: [String]) -> [UInt8] {
-    var bytesArray: [UInt8] = []
-    let length = stringArray.count
-    var littleEndianLength = length.littleEndian
-    let targetLength = 32
-    withUnsafeBytes(of: &littleEndianLength) {
-        bytesArray.append(contentsOf: $0)
-    }
-    for value in stringArray {
-        // TODO: should handle 254-bit input
-        var littleEndian = Int32(value)!.littleEndian
-        var byteLength = 0
-        withUnsafeBytes(of: &littleEndian) {
-            bytesArray.append(contentsOf: $0)
-            byteLength = byteLength + $0.count
-        }
-        if byteLength < targetLength {
-            let paddingCount = targetLength - byteLength
-            let paddingArray = [UInt8](repeating: 0, count: paddingCount)
-            bytesArray.append(contentsOf: paddingArray)
-        } 
-    }
-    return bytesArray
 }
