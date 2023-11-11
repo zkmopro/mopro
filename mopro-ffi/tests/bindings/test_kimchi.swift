d7a81e1f6e0f8c6bbb935ecda1760c1129203feb
oskarth <oskarth@titanproxy.com>
2023-11-11 22:12:17 +0300

    test(ffi): Kimchi test Swift bindings

--- mopro-ffi/tests/bindings/test_kimchi.swift
+++ mopro-ffi/tests/bindings/test_kimchi.swift
@@ -13,5 +13,28 @@ func testKimchiBench() {
     }
 }
 
+func testMoproKimchi() {
+    do {
+        // Create a new instance of MoproKimchi
+        let moproKimchi = MoproKimchi()
+
+        // Create a proof
+        let serializedProof = try moproKimchi.createProof()
+        print("Proof created successfully. Size: \(serializedProof.count)")
+
+        // Verify the proof
+        let isProofValid = try moproKimchi.verifyProof(proof: serializedProof)
+        print("Proof verification result: \(isProofValid)")
+
+    } catch let error as MoproError {
+        print("MoproError: \(error)")
+    } catch {
+        print("Unexpected error: \(error)")
+    }
+}
+
 // Execute the test
 testKimchiBench()
+
+// Execute the test
+testMoproKimchi()
