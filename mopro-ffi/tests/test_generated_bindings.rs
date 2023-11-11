9368715cc0c2cf910234c659c47eeb62d8455996
Meyanis95 <67648863+Meyanis95@users.noreply.github.com>
2023-11-11 14:14:34 +0300

    feat(core): Add RSA circuit example (#30)
    * Added RSA circuit to examples
    
    * fixed typo in readme
    
    * fixed newline + added swift rsa test

--- mopro-ffi/tests/test_generated_bindings.rs
+++ mopro-ffi/tests/test_generated_bindings.rs
@@ -5,4 +5,5 @@ uniffi::build_foreign_language_testcases!(
     //    "tests/bindings/test_mopro.py",
     "tests/bindings/test_mopro_keccak.swift",
     "tests/bindings/test_mopro_keccak2.swift",
+    "tests/bindings/test_mopro_rsa.swift",
 );
