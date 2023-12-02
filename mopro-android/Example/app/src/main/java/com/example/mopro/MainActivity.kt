8a1db593281d815b28e7b51d041f34b70da6f46f
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-12-02 20:22:50 +0300

    feat(android): build UI for generate keccak proof an verify proof

--- mopro-android/Example/app/src/main/java/com/example/mopro/MainActivity.kt
+++ mopro-android/Example/app/src/main/java/com/example/mopro/MainActivity.kt
@@ -3,51 +3,319 @@ package com.example.mopro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.mopro.ui.theme.moproTheme
+import uniffi.mopro.GenerateProofResult
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val inputs = mutableMapOf<String, List<String>>()
-        val a = 3
-        val b = 5
-        val c = a * b
-        inputs["a"] = listOf(a.toString())
-        inputs["b"] = listOf(b.toString())
-        var res = uniffi.mopro.generateProof2(inputs)
+
         setContent {
             moproTheme {
+
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting(res.toString())
-                }
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                ) { ProofComponent() }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "$name",
-        modifier = modifier
-    )
-}
+fun ProofComponent() {
+    var provingTime by remember { mutableStateOf("proving time:") }
+    var verifyingTime by remember { mutableStateOf("verifying time: ") }
+    var valid by remember { mutableStateOf("valid:") }
+    var res by remember {
+        mutableStateOf<GenerateProofResult>(
+                GenerateProofResult(proof = ByteArray(size = 0), inputs = ByteArray(size = 0))
+        )
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    moproTheme {
-        Greeting("Android")
+    val inputs = mutableMapOf<String, List<String>>()
+    inputs["in"] =
+            listOf(
+                    "0",
+                    "0",
+                    "1",
+                    "0",
+                    "1",
+                    "1",
+                    "1",
+                    "0",
+                    "1",
+                    "0",
+                    "1",
+                    "0",
+                    "0",
+                    "1",
+                    "1",
+                    "0",
+                    "1",
+                    "1",
+                    "0",
+                    "0",
+                    "1",
+                    "1",
+                    "1",
+                    "0",
+                    "0",
+                    "0",
+                    "1",
+                    "0",
+                    "1",
+                    "1",
+                    "1",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "0"
+            )
+
+    Box(modifier = Modifier.fillMaxSize().padding(16.dp), contentAlignment = Alignment.Center) {
+        Button(
+                onClick = {
+                    val startTime = System.currentTimeMillis()
+                    res = uniffi.mopro.generateProof2(inputs)
+                    val endTime = System.currentTimeMillis()
+                    provingTime = "proving time: " + (endTime - startTime).toString() + " ms"
+                }
+        ) { Text(text = "generate proof") }
+        Button(
+                onClick = {
+                    val startTime = System.currentTimeMillis()
+                    valid = "valid: " + uniffi.mopro.verifyProof2(res.proof, res.inputs).toString()
+                    val endTime = System.currentTimeMillis()
+                    verifyingTime = "verifying time: " + (endTime - startTime).toString() + " ms"
+                },
+                modifier = Modifier.padding(top = 100.dp)
+        ) { Text(text = "verify proof") }
+        Text(
+                text = "Keccak256 proof",
+                modifier = Modifier.padding(bottom = 80.dp),
+                fontWeight = FontWeight.Bold
+        )
+
+        Text(text = valid, modifier = Modifier.padding(top = 250.dp).width(200.dp))
+        Text(text = provingTime, modifier = Modifier.padding(top = 300.dp).width(200.dp))
+        Text(text = verifyingTime, modifier = Modifier.padding(top = 350.dp).width(200.dp))
     }
 }
