e81025869b24c519e6d4d04a2673d93fa05813ad
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-01-03 14:59:26 +0800

    chore(android): add android bindings tests

--- mopro-ffi/tests/test_generated_bindings.rs
+++ mopro-ffi/tests/test_generated_bindings.rs
@@ -1,11 +1,13 @@
 uniffi::build_foreign_language_testcases!(
     "tests/bindings/test_mopro.swift",
-    //    "tests/bindings/test_mopro.kts",
-    "tests/bindings/test_mopro_keccak2.kts",
+    "tests/bindings/test_mopro.kts",
     //    "tests/bindings/test_mopro.rb",
     //    "tests/bindings/test_mopro.py",
     "tests/bindings/test_mopro_keccak.swift",
+    "tests/bindings/test_mopro_keccak.kts",
     "tests/bindings/test_mopro_keccak2.swift",
-    // "tests/bindings/test_mopro_rsa.swift",
+    "tests/bindings/test_mopro_keccak2.kts",
+    "tests/bindings/test_mopro_rsa.swift",
+    "tests/bindings/test_mopro_rsa.kts",
     // "tests/bindings/test_mopro_rsa2.swift",
 );
