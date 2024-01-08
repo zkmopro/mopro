fc420401727141b8e0e25fedbc70cc30994f5c68
oskarth <oskarth@titanproxy.com>
2024-01-08 12:50:48 +0800

    feat(cli): Init mopro CLI stub

---
+++ mopro-cli/README.md
@@ -0,0 +1,22 @@
+# mopro-cli
+
+`mopro` is a CLI tool for client-side proving of Zero Knowledge Proofs. It simplifies the process of initializing, building, updating, and testing projects across different platforms and configurations.
+
+Think of it as Foundry for client-side proving.
+
+## Installation
+
+To use `mopro`, you need to have Rust and Cargo installed on your system. You can install them from [the official Rust website](https://www.rust-lang.org/learn/get-started).
+
+## Usage
+
+Here are the basic commands of `mopro`:
+
+- `mopro init`: Initialize a new project with support for multiple platforms.
+- `mopro build`: Build the project for specified platforms.
+- `mopro update`: Update bindings with new API for specified platforms.
+- `mopro test`: Run tests for specific platform and test cases.
+
+## Contributing
+
+Contributions to `mopro` are welcome. Please feel free to submit issues and pull requests.
---
+++ mopro-cli/src/main.rs
@@ -0,0 +1,85 @@
+use clap::{Parser, Subcommand};
+
+/// CLI for multi-platform project management
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Initializes a new project
+    Init {
+        #[arg(long)]
+        adapter: String,
+        #[arg(long)]
+        platform: String,
+        project_name: String,
+    },
+    /// Builds the project for specified platforms
+    Build {
+        #[arg(long)]
+        adapter: String,
+        #[arg(long)]
+        platform: String,
+    },
+    /// Updates bindings for the specified platforms
+    Update {
+        #[arg(long)]
+        adapter: String,
+        #[arg(long)]
+        platform: String,
+    },
+    /// Runs tests for the specified platform and test cases
+    Test {
+        #[arg(long)]
+        adapter: String,
+        #[arg(long)]
+        platform: String,
+        #[arg(long)]
+        test_case: String,
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Init {
+            adapter,
+            platform,
+            project_name,
+        } => {
+            println!(
+                "Initializing project: {}, {}, {}",
+                adapter, platform, project_name
+            );
+            // Implement initialization logic here
+            println!("Not yet implemented")
+        }
+        Commands::Build { adapter, platform } => {
+            println!("Building project for platform {}: {}", platform, adapter);
+            // Implement build logic here
+            println!("Not yet implemented")
+        }
+        Commands::Update { adapter, platform } => {
+            println!("Updating project for platform {}: {}", platform, adapter);
+            // Implement update logic here
+            println!("Not yet implemented")
+        }
+        Commands::Test {
+            adapter,
+            platform,
+            test_case,
+        } => {
+            println!(
+                "Testing project on platform {} with test {}: {}",
+                platform, test_case, adapter
+            );
+            // Implement test logic here
+            println!("Not yet implemented")
+        }
+    }
+}
