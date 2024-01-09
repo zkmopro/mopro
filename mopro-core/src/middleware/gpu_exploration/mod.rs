ed4641b93e698c7f450c8701ed3d1504360d09de
moven0831 <moven0831@gmail.com>
2024-01-09 21:48:50 +0800

    feat(msm): add memory usage benchmarking

--- mopro-core/src/middleware/gpu_exploration/mod.rs
+++ mopro-core/src/middleware/gpu_exploration/mod.rs
@@ -4,14 +4,19 @@ use ark_std::{UniformRand, error::Error};
 
 // For benchmarking
 use std::time::{Duration, Instant};
+use jemalloc_ctl::{stats, epoch};
+
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 fn single_msm() -> Result<(), Box<dyn Error>> {
+    epoch::advance().unwrap();  // Update jemalloc stats
+
     let mut rng = ark_std::test_rng();
     
     // We use the BN254 curve to match Groth16 proving system
     let a = GAffine::rand(&mut rng);
     let b = GAffine::rand(&mut rng);
-    
     let s1 = ScalarField::rand(&mut rng);
     let s2 = ScalarField::rand(&mut rng);
     
@@ -25,17 +30,28 @@ fn single_msm() -> Result<(), Box<dyn Error>> {
 pub fn run_msm_bench(num_msm: Option<usize>) -> Result<(), Box<dyn Error>> {
     let num_msm = num_msm.unwrap_or(1000);    // default to 1000 msm operations
 
+    let allocated_before = stats::allocated::read().unwrap();
+    let resident_before = stats::resident::read().unwrap();
+
     let mut total_msm = Duration::new(0, 0);
+
     for _ in 0..num_msm {
         let start = Instant::now();
         single_msm()?;
         total_msm += start.elapsed();
     }
 
+    let allocated_after = stats::allocated::read().unwrap();
+    let resident_after = stats::resident::read().unwrap();
+
+    let allocated_mem = (allocated_after - allocated_before) as f64 / 1_000_000f64;
+    let resident_mem = (resident_after - resident_before) as f64 / 1_000_000f64;
     let msm_avg = total_msm / num_msm.try_into().unwrap();
     let msm_avg = msm_avg.subsec_nanos() as f64 / 1_000_000_000f64 + (msm_avg.as_secs() as f64);
     
     println!("\nBenchmarking {:?} msm on BN254 curve", num_msm);
     println!("└─ Average msm time: {:?} seconds\n└─ Overall processing time: {:?}", msm_avg, total_msm);
+    println!("└─ Memory allocated: {} MB", allocated_mem);
+    println!("└─ Resident memory: {} MB", resident_mem);
     Ok(())
 }
