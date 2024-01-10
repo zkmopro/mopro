9afd277b24be72c81bdd33694c191ea27c416c26
moven0831 <moven0831@gmail.com>
2024-01-10 19:22:01 +0800

    refactor(msm_bench): restructure memory usage part

--- mopro-core/src/middleware/gpu_exploration/mod.rs
+++ mopro-core/src/middleware/gpu_exploration/mod.rs
@@ -9,9 +9,7 @@ use jemalloc_ctl::{stats, epoch};
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
-fn single_msm() -> Result<(), Box<dyn Error>> {
-    epoch::advance().unwrap();  // Update jemalloc stats
-
+fn single_msm() -> Result<(), Box<dyn Error>> {    
     let mut rng = ark_std::test_rng();
     
     // We use the BN254 curve to match Groth16 proving system
@@ -30,28 +28,28 @@ fn single_msm() -> Result<(), Box<dyn Error>> {
 pub fn run_msm_bench(num_msm: Option<u32>) -> Result<(), Box<dyn Error>> {
     let num_msm = num_msm.unwrap_or(1000);    // default to 1000 msm operations
 
-    let allocated_before = stats::allocated::read().unwrap();
-    let resident_before = stats::resident::read().unwrap();
+    let mem_epoch = epoch::mib().unwrap();  // For updating jemalloc stats of memory usage
+    let allocated = stats::allocated::mib().unwrap();
+    let resident = stats::resident::mib().unwrap();
 
     let mut total_msm = Duration::new(0, 0);
-
+    
     for _ in 0..num_msm {
         let start = Instant::now();
         single_msm()?;
         total_msm += start.elapsed();
     }
-
-    let allocated_after = stats::allocated::read().unwrap();
-    let resident_after = stats::resident::read().unwrap();
-
-    let allocated_mem = (allocated_after - allocated_before) as f64 / 1_000_000f64;
-    let resident_mem = (resident_after - resident_before) as f64 / 1_000_000f64;
+    mem_epoch.advance().unwrap();  // Update jemalloc stats
+    
+    let allocated_size = allocated.read().unwrap() as f64 / usize::pow(1_024, 2) as f64;  // Convert to MiB
+    let resident_size = resident.read().unwrap() as f64 / usize::pow(1_024, 2) as f64;   // Convert to MiB
+    
     let msm_avg = total_msm / num_msm.try_into().unwrap();
     let msm_avg = msm_avg.subsec_nanos() as f64 / 1_000_000_000f64 + (msm_avg.as_secs() as f64);
     
     println!("\nBenchmarking {:?} msm on BN254 curve", num_msm);
     println!("└─ Average msm time: {:?} seconds\n└─ Overall processing time: {:?}", msm_avg, total_msm);
-    println!("└─ Memory allocated: {} MB", allocated_mem);
-    println!("└─ Resident memory: {} MB", resident_mem);
+    println!("└─ Memory allocated: {} MiB", allocated_size);
+    println!("└─ Resident memory: {} MiB", resident_size);
     Ok(())
 }
