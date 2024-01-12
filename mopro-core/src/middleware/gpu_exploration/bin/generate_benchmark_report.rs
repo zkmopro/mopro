642f0ecf96de6c9747ed1b1acd86c7b726bd79ec
moven0831 <moven0831@gmail.com>
2024-01-12 16:33:20 +0800

    feat(msm_bench): generate report for msm benchmarking

---
+++ mopro-core/src/middleware/gpu_exploration/bin/generate_benchmark_report.rs
@@ -0,0 +1,27 @@
+// This file is used to generate the benchmark report for the GPU exploration middleware.
+
+use std::{env, fs::File, io::Write};
+
+use mopro_core::middleware::gpu_exploration::run_msm_benchmark;
+
+fn main() {
+    let path = env::current_dir()
+        .unwrap()
+        .join("src/middleware/gpu_exploration/msm_bench.csv");
+    let mut file = File::create(path).unwrap();
+    writeln!(file, "num_msm,avg_processing_time(sec),total_processing_time(sec),memory_allocated(MiB),resident_memory(MiB)").unwrap();
+    let trials = vec![1, 10, 50, 100, 500, 1_000, 5_000];
+    for each in trials {
+        let bench_data = run_msm_benchmark(Some(each)).unwrap();
+        writeln!(
+            file,
+            "{},{},{},{},{}",
+            bench_data.num_msm,
+            bench_data.avg_processing_time,
+            bench_data.total_processing_time,
+            bench_data.allocated_memory,
+            bench_data.resident_memory
+        )
+        .unwrap();
+    }
+}
