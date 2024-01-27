565f9ac8f45c3d81b4bf62feefe1afa65d8ea1c2
Fu-Chuan Chung <sh1001309@gmail.com>
2024-01-27 21:12:28 +0000

    fix: add gpu-benchmarks feature flag

--- mopro-ffi/tests/test_generated_bindings.rs
+++ mopro-ffi/tests/test_generated_bindings.rs
@@ -1,6 +1,22 @@
+#[cfg(feature = "gpu-benchmarks")]
 uniffi::build_foreign_language_testcases!(
-    // "tests/bindings/test_mopro_gen_benchmarks_report.swift", // uncomment this to generate the benchmarks report in swift
-    "tests/bindings/test_mopro_gpu_benchmarks.swift",
+    "tests/bindings/test_mopro_gen_benchmarks_report.swift",
+    "tests/bindings/test_mopro_gpu_benchmarks.swift",    
+    "tests/bindings/test_mopro.swift",
+    "tests/bindings/test_mopro.kts",
+    //    "tests/bindings/test_mopro.rb",
+    //    "tests/bindings/test_mopro.py",
+    "tests/bindings/test_mopro_keccak.swift",
+    // "tests/bindings/test_mopro_keccak.kts", // FIXME: java.lang.OutOfMemoryError: Java heap space
+    "tests/bindings/test_mopro_keccak2.swift",
+    "tests/bindings/test_mopro_keccak2.kts",
+    "tests/bindings/test_mopro_rsa.swift",
+    // "tests/bindings/test_mopro_rsa.kts", // FIXME: java.lang.OutOfMemoryError: Java heap space
+    // "tests/bindings/test_mopro_rsa2.swift",
+);
+
+#[cfg(not(feature = "gpu-benchmarks"))]
+uniffi::build_foreign_language_testcases!(
     "tests/bindings/test_mopro.swift",
     "tests/bindings/test_mopro.kts",
     //    "tests/bindings/test_mopro.rb",
