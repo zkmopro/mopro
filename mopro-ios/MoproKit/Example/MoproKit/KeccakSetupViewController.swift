ef75e431a98a819e35c5d1336750ad847a52227a
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-02-22 20:10:07 +0800

    feat(ios): download arkzkey and wasm from server and generate proof

--- mopro-ios/MoproKit/Example/MoproKit/KeccakSetupViewController.swift
+++ mopro-ios/MoproKit/Example/MoproKit/KeccakSetupViewController.swift
@@ -6,189 +6,220 @@
 //  Copyright (c) 2023 1552237. All rights reserved.
 //
 
-import UIKit
 import MoproKit
+import UIKit
 
 class KeccakSetupViewController: UIViewController {
 
-    var setupButton = UIButton(type: .system)
-    var proveButton = UIButton(type: .system)
-    var verifyButton = UIButton(type: .system)
-    var textView = UITextView()
+  let arkzkeyUrl = URL(string: "https://mopro.vivianjeng.xyz/keccak256_256_test_final.arkzkey")
+  let wasmUrl = URL(string: "https://mopro.vivianjeng.xyz/keccak256_256_test.wasm")
 
-    let moproCircom = MoproKit.MoproCircom()
-    var generatedProof: Data?
-    var publicInputs: Data?
+  var downloadButton = UIButton(type: .system)
+  var setupButton = UIButton(type: .system)
+  var proveButton = UIButton(type: .system)
+  var verifyButton = UIButton(type: .system)
+  var textView = UITextView()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  let moproCircom = MoproKit.MoproCircom()
+  var generatedProof: Data?
+  var publicInputs: Data?
 
-        // Set title
-        let title = UILabel()
-        title.text = "Keccak256 (setup)"
-        title.textColor = .white
-        title.textAlignment = .center
-        navigationItem.titleView = title
-        navigationController?.navigationBar.isHidden = false
-        navigationController?.navigationBar.prefersLargeTitles = true
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        // view.backgroundColor = .white
-        // navigationController?.navigationBar.prefersLargeTitles = true
-        // navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
-        // navigationController?.navigationBar.barTintColor = UIColor.white // or any other contrasting color
-        // self.title = "Keccak256 (setup)"
+    // Set title
+    let title = UILabel()
+    title.text = "Keccak256 (setup)"
+    title.textColor = .white
+    title.textAlignment = .center
+    navigationItem.titleView = title
+    navigationController?.navigationBar.isHidden = false
+    navigationController?.navigationBar.prefersLargeTitles = true
 
-        setupUI()
+    // view.backgroundColor = .white
+    // navigationController?.navigationBar.prefersLargeTitles = true
+    // navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+    // navigationController?.navigationBar.barTintColor = UIColor.white // or any other contrasting color
+    // self.title = "Keccak256 (setup)"
+
+    setupUI()
+  }
+
+  func setupUI() {
+    downloadButton.setTitle("Download Ark Zkey", for: .normal)
+    setupButton.setTitle("Setup", for: .normal)
+    proveButton.setTitle("Prove", for: .normal)
+    verifyButton.setTitle("Verify", for: .normal)
+
+    textView.isEditable = false
+
+    //self.title = "Keccak256 (setup)"
+    //view.backgroundColor = .black
+
+    // Setup actions for buttons
+    downloadButton.addTarget(self, action: #selector(runDownloadAction), for: .touchUpInside)
+    setupButton.addTarget(self, action: #selector(runSetupAction), for: .touchUpInside)
+    proveButton.addTarget(self, action: #selector(runProveAction), for: .touchUpInside)
+    verifyButton.addTarget(self, action: #selector(runVerifyAction), for: .touchUpInside)
+
+    downloadButton.contentEdgeInsets = UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 16)
+    setupButton.contentEdgeInsets = UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 16)
+    proveButton.contentEdgeInsets = UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 16)
+    verifyButton.contentEdgeInsets = UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 16)
+
+    let stackView = UIStackView(arrangedSubviews: [
+      downloadButton, setupButton, proveButton, verifyButton, textView,
+    ])
+    stackView.axis = .vertical
+    stackView.spacing = 10
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(stackView)
+
+    // Make text view visible
+    textView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+
+    NSLayoutConstraint.activate([
+      stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+    ])
+  }
+
+  @objc func runDownloadAction() {
+    let arkzkeyStart = CFAbsoluteTimeGetCurrent()
+    FileDownloader.loadFileAsync(url: self.arkzkeyUrl!) { (path, error) in
+      print("Ark zkey File downloaded to : \(path!)")
+      let arkzkeyEnd = CFAbsoluteTimeGetCurrent()
+      print("Download ark key took:", arkzkeyEnd - arkzkeyStart)
     }
 
-    func setupUI() {
-        setupButton.setTitle("Setup", for: .normal)
-        proveButton.setTitle("Prove", for: .normal)
-        verifyButton.setTitle("Verify", for: .normal)
-
-       textView.isEditable = false
-
-        //self.title = "Keccak256 (setup)"
-        //view.backgroundColor = .black
-
-        // Setup actions for buttons
-        setupButton.addTarget(self, action: #selector(runSetupAction), for: .touchUpInside)
-        proveButton.addTarget(self, action: #selector(runProveAction), for: .touchUpInside)
-        verifyButton.addTarget(self, action: #selector(runVerifyAction), for: .touchUpInside)
-
-       setupButton.contentEdgeInsets = UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 16)
-       proveButton.contentEdgeInsets = UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 16)
-       verifyButton.contentEdgeInsets = UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 16)
-
-        let stackView = UIStackView(arrangedSubviews: [setupButton, proveButton, verifyButton, textView])
-        stackView.axis = .vertical
-        stackView.spacing = 10
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(stackView)
-
-        // Make text view visible
-        textView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-
-        NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
-        ])
+    let wasmStart = CFAbsoluteTimeGetCurrent()
+    FileDownloader.loadFileAsync(url: self.wasmUrl!) { (path, error) in
+      print("wasm File downloaded to : \(path!)")
+      let wasmEnd = CFAbsoluteTimeGetCurrent()
+      print("Download wasm took:", wasmEnd - wasmStart)
     }
 
-    @objc func runSetupAction() {
-        // Logic for setup
-        if let arkzkeyPath = Bundle.main.path(forResource: "keccak256_256_test_final", ofType: "arkzkey"),
-            let wasmPath = Bundle.main.path(forResource: "keccak256_256_test", ofType: "wasm") {
+  }
 
-       // Multiplier example
-       // if let wasmPath = Bundle.main.path(forResource: "multiplier2", ofType: "wasm"),
-       //    let r1csPath = Bundle.main.path(forResource: "multiplier2", ofType: "r1cs") {
+  @objc func runSetupAction() {
+    // Logic for setup
+    if let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+      .first
+    {
 
-           do {
-            textView.text += "Initializing library\n"
-            // Record start time
-            let start = CFAbsoluteTimeGetCurrent()
+      let arkzkeyPath = documentsUrl.appendingPathComponent((arkzkeyUrl!).lastPathComponent)
+      let wasmPath = documentsUrl.appendingPathComponent((wasmUrl!).lastPathComponent)
 
-            try moproCircom.initialize(arkzkeyPath: arkzkeyPath, wasmPath: wasmPath)
-            proveButton.isEnabled = true // Enable the Prove button upon successful setup
+      // Multiplier example
+      // if let wasmPath = Bundle.main.path(forResource: "multiplier2", ofType: "wasm"),
+      //    let r1csPath = Bundle.main.path(forResource: "multiplier2", ofType: "r1cs") {
 
-            // Record end time and compute duration
-            let end = CFAbsoluteTimeGetCurrent()
-            let timeTaken = end - start
+      do {
+        textView.text += "Initializing library\n"
+        // Record start time
+        let start = CFAbsoluteTimeGetCurrent()
 
-            textView.text += "Initializing arkzkey and wasm took \(timeTaken) seconds.\n"
-           } catch let error as MoproError {
-               print("MoproError: \(error)")
-           } catch {
-               print("Unexpected error: \(error)")
-           }
-       } else {
-           print("Error getting paths for resources")
-       }
+        try moproCircom.initialize(arkzkeyPath: arkzkeyPath.path, wasmPath: wasmPath.path)
+        proveButton.isEnabled = true  // Enable the Prove button upon successful setup
+
+        // Record end time and compute duration
+        let end = CFAbsoluteTimeGetCurrent()
+        let timeTaken = end - start
+
+        textView.text += "Initializing arkzkey and wasm took \(timeTaken) seconds.\n"
+      } catch let error as MoproError {
+        print("MoproError: \(error)")
+      } catch {
+        print("Unexpected error: \(error)")
+      }
+    } else {
+      print("Error getting paths for resources")
     }
+  }
 
-    @objc func runProveAction() {
-        // Logic for prove
-        guard proveButton.isEnabled else {
-           print("Setup is not completed yet.")
-           return
-        }
-        do {
-            // Prepare inputs
-            let inputVec: [UInt8] = [
-                116, 101, 115, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0,
-            ]
-            let bits = bytesToBits(bytes: inputVec)
-            var inputs = [String: [String]]()
-            inputs["in"] = bits
-
-            // Multiplier example
-            // var inputs = [String: [String]]()
-            // let a = 3
-            // let b = 5
-            // inputs["a"] = [String(a)]
-            // inputs["b"] = [String(b)]
-
-            // Record start time
-            let start = CFAbsoluteTimeGetCurrent()
-
-            // Generate Proof
-            let generateProofResult = try moproCircom.generateProof(circuitInputs: inputs)
-            assert(!generateProofResult.proof.isEmpty, "Proof should not be empty")
-
-            // Record end time and compute duration
-            let end = CFAbsoluteTimeGetCurrent()
-            let timeTaken = end - start
-
-            // Store the generated proof and public inputs for later verification
-            generatedProof = generateProofResult.proof
-            publicInputs = generateProofResult.inputs
-
-            textView.text += "Proof generation took \(timeTaken) seconds.\n"
-            verifyButton.isEnabled = true // Enable the Verify button once proof has been generated
-        } catch let error as MoproError {
-            print("MoproError: \(error)")
-        } catch {
-            print("Unexpected error: \(error)")
-        }
+  @objc func runProveAction() {
+    // Logic for prove
+    guard proveButton.isEnabled else {
+      print("Setup is not completed yet.")
+      return
     }
+    do {
+      // Prepare inputs
+      let inputVec: [UInt8] = [
+        116, 101, 115, 116, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,
+      ]
+      let bits = bytesToBits(bytes: inputVec)
+      var inputs = [String: [String]]()
+      inputs["in"] = bits
 
-    @objc func runVerifyAction() {
-        // Logic for verify
-        guard let proof = generatedProof,
-                let publicInputs = publicInputs else {
-            print("Setup is not completed or proof has not been generated yet.")
-            return
-        }
-        do {
-            // Verify Proof
-            let isValid = try moproCircom.verifyProof(proof: proof, publicInput: publicInputs)
-            assert(isValid, "Proof verification should succeed")
+      // Multiplier example
+      // var inputs = [String: [String]]()
+      // let a = 3
+      // let b = 5
+      // inputs["a"] = [String(a)]
+      // inputs["b"] = [String(b)]
 
-            textView.text += "Proof verification succeeded.\n"
-        } catch let error as MoproError {
-            print("MoproError: \(error)")
-        } catch {
-            print("Unexpected error: \(error)")
-        }
+      // Record start time
+      let start = CFAbsoluteTimeGetCurrent()
+
+      // Generate Proof
+      let generateProofResult = try moproCircom.generateProof(circuitInputs: inputs)
+      assert(!generateProofResult.proof.isEmpty, "Proof should not be empty")
+
+      // Record end time and compute duration
+      let end = CFAbsoluteTimeGetCurrent()
+      let timeTaken = end - start
+
+      // Store the generated proof and public inputs for later verification
+      generatedProof = generateProofResult.proof
+      publicInputs = generateProofResult.inputs
+
+      textView.text += "Proof generation took \(timeTaken) seconds.\n"
+      verifyButton.isEnabled = true  // Enable the Verify button once proof has been generated
+    } catch let error as MoproError {
+      print("MoproError: \(error)")
+    } catch {
+      print("Unexpected error: \(error)")
     }
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  @objc func runVerifyAction() {
+    // Logic for verify
+    guard let proof = generatedProof,
+      let publicInputs = publicInputs
+    else {
+      print("Setup is not completed or proof has not been generated yet.")
+      return
     }
+    do {
+      // Verify Proof
+      let isValid = try moproCircom.verifyProof(proof: proof, publicInput: publicInputs)
+      assert(isValid, "Proof verification should succeed")
+
+      textView.text += "Proof verification succeeded.\n"
+    } catch let error as MoproError {
+      print("MoproError: \(error)")
+    } catch {
+      print("Unexpected error: \(error)")
+    }
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 }
 
 func bytesToBits(bytes: [UInt8]) -> [String] {
-    var bits = [String]()
-    for byte in bytes {
-        for j in 0..<8 {
-            let bit = (byte >> j) & 1
-            bits.append(String(bit))
-        }
+  var bits = [String]()
+  for byte in bytes {
+    for j in 0..<8 {
+      let bit = (byte >> j) & 1
+      bits.append(String(bit))
     }
-    return bits
+  }
+  return bits
 }
