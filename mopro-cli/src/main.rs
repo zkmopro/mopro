07d8c1974f419b7e99d40000af9480a22af2eaf3
oskarth <oskarth@titanproxy.com>
2024-02-29 14:03:26 +0800

    feat(cli): Basic build script

--- mopro-cli/src/main.rs
+++ mopro-cli/src/main.rs
@@ -1,6 +1,8 @@
 use clap::{Parser, Subcommand};
-
+use std::process::Command;
 mod init;
+use std::env;
+use std::process;
 
 /// CLI for multi-platform project management
 #[derive(Parser, Debug)]
@@ -24,8 +26,10 @@ enum Commands {
     /// Builds the project for specified platforms
     Build {
         #[arg(long)]
+        config: String,
+        #[arg(long, default_value = "circom")]
         adapter: String,
-        #[arg(long)]
+        #[arg(long, default_value = "desktop")]
         platform: String,
     },
     /// Updates bindings for the specified platforms
@@ -69,10 +73,50 @@ fn main() {
                 println!("Not yet implemented")
             }
         }
-        Commands::Build { adapter, platform } => {
-            println!("Building project for platform {}: {}", platform, adapter);
-            // Implement build logic here
-            println!("Not yet implemented")
+        Commands::Build {
+            config,
+            adapter,
+            platform,
+        } => {
+            println!(
+                "Building project for platform {}: {} with config {}",
+                platform, adapter, config
+            );
+
+            // Attempt to retrieve the MOPRO_ROOT environment variable
+            match env::var("MOPRO_ROOT") {
+                Ok(mopro_root) => {
+                    // Construct the path to the build script using the MOPRO_ROOT environment variable
+                    let script_path = format!("{}/scripts/build_minimal_project.sh", mopro_root);
+
+                    // NOTE: When we feel more confident in this we can turn down the verbosity
+                    // For example by capturing output and only printing it if the build fails
+                    //
+                    // Execute the shell script with the provided config file as an argument
+                    let status = Command::new("sh")
+                        .arg(script_path)
+                        .arg(&config) // Pass the config file path as an argument to the script
+                        .status()
+                        //.output() // NOTE: This captures standard output
+                        .expect("Failed to execute build script");
+
+                    if status.success() {
+                        println!("Cargo build completed successfully.");
+                    } else {
+                        eprintln!("Cargo build failed.");
+                        process::exit(1);
+                    }
+                }
+                Err(_) => {
+                    eprintln!("Error: MOPRO_ROOT environment variable is not set.");
+                    eprintln!("Please set MOPRO_ROOT to point to the local checkout of mopro.");
+                    eprintln!(
+                        "For example: export MOPRO_ROOT=/Users/user/repos/github.com/oskarth/mopro"
+                    );
+                    eprintln!("Git repository: https://github.com/oskarth/mopro");
+                    process::exit(1);
+                }
+            }
         }
         Commands::Update { adapter, platform } => {
             println!("Updating project for platform {}: {}", platform, adapter);
