7fd145162bb61522f0efecc4a1a79b33cb0e13ec
oskarth <oskarth@titanproxy.com>
2024-02-29 14:50:00 +0800

    feat(cli): Improve mopro init

--- mopro-cli/src/main.rs
+++ mopro-cli/src/main.rs
@@ -1,7 +1,10 @@
 use clap::{Parser, Subcommand};
 use std::process::Command;
 mod init;
+use fs_extra::dir::{self, CopyOptions};
 use std::env;
+use std::fs;
+use std::path::PathBuf;
 use std::process;
 
 /// CLI for multi-platform project management
@@ -60,18 +63,62 @@ fn main() {
             project_name,
         } => {
             println!(
-                "Initializing project: {}, {}, {}",
-                adapter, platform, project_name
+                "Init project for platform {}: {} and name {}",
+                platform, adapter, project_name
             );
-            // Implement initialization logic here
-            // If adapter is 'circom', platform is 'desktop', then do something
-            if adapter == "circom" && platform == "desktop" {
-                println!("Initializing circom project for desktop");
-                init::create_project_structure(project_name);
-                println!("Project {} created successfully", project_name);
+
+            let mopro_root =
+                env::var("MOPRO_ROOT").expect("MOPRO_ROOT environment variable is not set");
+            let source_path = PathBuf::from(mopro_root).join("mopro-cli-example");
+            let current_dir = env::current_dir().expect("Failed to get current directory");
+            let destination_path = current_dir.join(project_name);
+
+            // Create the project directory
+            if destination_path.exists() {
+                eprintln!(
+                    "A directory with the name '{}' already exists.",
+                    project_name
+                );
+                std::process::exit(1);
             } else {
-                println!("Not yet implemented")
+                fs::create_dir(&destination_path).expect("Failed to create project directory");
             }
+
+            let mut options = CopyOptions::new();
+            options.overwrite = true;
+            options.copy_inside = true;
+
+            // List of directories and files to copy
+            let entries_to_copy = fs::read_dir(&source_path)
+                .expect("Failed to read source directory")
+                .filter_map(|entry| {
+                    let entry = entry.expect("Failed to read directory entry");
+                    let path = entry.path();
+                    let file_name = path.file_name().unwrap().to_str().unwrap();
+                    // Exclude the `ptau` and `target` directories
+                    if file_name != "ptau" && file_name != "target" {
+                        Some(path)
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<PathBuf>>();
+
+            // Perform the copy operation for each entry
+            for entry in entries_to_copy {
+                if entry.is_dir() {
+                    // Copy directory
+                    dir::copy(entry, &destination_path, &options)
+                        .expect("Failed to copy directory");
+                } else {
+                    // Copy file
+                    let file_name = entry.file_name().unwrap();
+                    fs::copy(&entry, destination_path.join(file_name))
+                        .expect("Failed to copy file");
+                }
+            }
+
+            println!("Project initialized successfully.");
         }
         Commands::Build {
             config,
