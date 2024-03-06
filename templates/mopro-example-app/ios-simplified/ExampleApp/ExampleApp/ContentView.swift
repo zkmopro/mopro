c08286b851997692bca3f59fee50bae70a83437f
oskarth <oskarth@titanproxy.com>
2024-03-06 13:21:43 +0800

    feat(ios-simplified): Init example app

---
+++ templates/mopro-example-app/ios-simplified/ExampleApp/ExampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExampleApp
+//
+//  Created by User Name on 3/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
