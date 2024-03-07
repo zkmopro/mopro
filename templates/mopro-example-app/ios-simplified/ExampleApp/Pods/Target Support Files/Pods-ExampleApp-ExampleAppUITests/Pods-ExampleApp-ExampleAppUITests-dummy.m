429ee34ad325c341f4e3a7bc80a21888c11ebe9f
oskarth <oskarth@titanproxy.com>
2024-03-07 12:16:14 +0800

    chore(ios-simplified): Update gitignored files

--- templates/mopro-example-app/ios-simplified/ExampleApp/Pods/Target Support Files/Pods-ExampleApp-ExampleAppUITests/Pods-ExampleApp-ExampleAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ExampleApp_ExampleAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ExampleApp_ExampleAppUITests
-@end
--- templates/mopro-example-app/ios-simplified/ExampleApp/Pods/Target Support Files/Pods-ExampleAppTests/Pods-ExampleAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ExampleAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_ExampleAppTests
-@end
