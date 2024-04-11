e025f08d7f1a9b96dbfe4ed2179d56aa2160b8c0
moven0831 <moven0831@gmail.com>
2024-04-12 00:49:46 +0800

    feat(baseline): adopt zprize benchmarking method and enable multi benchmarking at once

--- mopro-core/src/middleware/gpu_explorations/arkworks_pippenger.rs
+++ mopro-core/src/middleware/gpu_explorations/arkworks_pippenger.rs
@@ -1,39 +1,13 @@
 use ark_bls12_377::{Fr as ScalarField, G1Affine, G1Projective};
-use ark_bls12_377_3;
-use ark_ec::short_weierstrass::{Affine, SWCurveConfig};
-use ark_ff::{BigInt, Field, PrimeField};
-// use ark_bn254::g1::G1Affine;
 // use ark_bn254::{Fr as ScalarField, FrConfig, G1Affine as GAffine, G1Projective as G};
-use ark_ec::{AffineRepr, CurveGroup, VariableBaseMSM};
-use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Write};
-use ark_std::rand::{Rng, RngCore};
-use ark_std::{error::Error, UniformRand, Zero};
+use ark_ec::{AffineRepr, VariableBaseMSM};
+use ark_ff::{BigInt, Field, FpConfig};
+use ark_serialize::Write;
 
-use crate::middleware::gpu_explorations::utils::preprocess;
+use crate::middleware::gpu_explorations::utils::{benchmark::BenchmarkResult, preprocess};
 
 use std::fs::File;
 use std::time::{Duration, Instant};
-
-#[derive(Debug, Clone)]
-pub struct BenchmarkResult {
-    pub num_msm: u32,
-    pub avg_processing_time: f64,
-    pub total_processing_time: f64,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct MyAffine<P: SWCurveConfig>(pub Affine<P>);
-
-impl<P: SWCurveConfig> From<([u8; 32], [u8; 32])> for MyAffine<P> {
-    fn from(bytes: ([u8; 32], [u8; 32])) -> Self {
-        // Your conversion logic here
-        // Let's say you somehow convert bytes to x and y...
-        let x = P::BaseField::from_random_bytes(&bytes.0).unwrap();
-        let y = P::BaseField::from_random_bytes(&bytes.1).unwrap();
-
-        MyAffine(Affine::new(x, y))
-    }
-}
 
 pub fn benchmark_msm<I>(
     benchmark_dir: &str,
@@ -43,113 +17,138 @@ pub fn benchmark_msm<I>(
 where
     I: Iterator<Item = preprocess::Instance>,
 {
-    let output_path = format!("{}{}", benchmark_dir, "/arkworks_pippenger_time.txt");
-    let mut output_file = File::create(output_path).expect("output file creation failed");
-    let mut result_vec = Vec::new();
+    let mut instance_durations = Vec::new();
 
     for instance in instances {
-        let points_slice: Vec<G1Affine> = instance
-            .0
-            .iter()
-            .map(|&p| {
-                let mut bytes = Vec::new();
-                <SWCurveConfig as Into<MyAffine>>::into(p)
-                    .serialize_uncompressed(&mut bytes)
-                    .unwrap(); // use custom from implementation
-                               // p.into().serialize_uncompressed(&mut bytes).unwrap();   // use custom from implementation
-                println!("p: {:?}", p.to_string());
-                G1Affine::deserialize_uncompressed_unchecked(&*bytes).unwrap()
-            })
-            .collect();
+        let points = &instance.0;
+        let scalars = &instance.1;
+        let mut parsed_points = Vec::<G1Affine>::new();
+        let mut parsed_scalars = Vec::<ScalarField>::new();
 
-        let scalars_slice: Vec<ScalarField> = instance
-            .1
-            .iter()
-            .map(|&s| {
-                let mut bytes = Vec::new();
-                s.to_string().serialize_uncompressed(&mut bytes).unwrap();
-
-                ScalarField::deserialize_uncompressed_unchecked(&*bytes).unwrap()
-            })
-            .collect();
-
-        let mut total_duration = Duration::ZERO;
-        for i in 0..iterations {
-            let start = Instant::now();
-            let result = <G1Projective as VariableBaseMSM>::msm(&points_slice, &scalars_slice);
-            total_duration += start.elapsed();
+        // parse points and scalars from arkworks 0.3 compatible format to 0.4 compatible
+        for p in points {
+            let new_p =
+                G1Affine::new_unchecked(BigInt::new(p.x.0 .0).into(), BigInt::new(p.y.0 .0).into());
+            parsed_points.push(new_p);
         }
 
-        let avg_duration = total_duration / iterations;
-        writeln!(
-            output_file,
-            "Average duration across all iterations: {:?}",
-            avg_duration
-        )?;
+        for s in scalars {
+            let new_s = ScalarField::new(BigInt::new(s.0));
+            parsed_scalars.push(new_s);
+        }
+
+        let mut instance_total_duration = Duration::ZERO;
+        for i in 0..iterations {
+            let start = Instant::now();
+            let result =
+                <G1Projective as VariableBaseMSM>::msm(&parsed_points[..], &parsed_scalars[..])
+                    .unwrap();
+
+            instance_total_duration += start.elapsed();
+        }
+        let instance_avg_duration = instance_total_duration / iterations;
+
         println!(
-            "Average time to execute MSM with {} points and {} scalars and {} iterations is: {:?}",
-            points_slice.len(),
-            scalars_slice.len(),
+            "Average time to execute MSM with {} points and scalars in {} iterations is: {:?}",
+            points.len(),
             iterations,
-            avg_duration
+            instance_avg_duration,
         );
-        result_vec.push(avg_duration);
+        instance_durations.push(instance_avg_duration);
     }
-    Ok(result_vec)
+    Ok(instance_durations)
 }
 
-pub fn run_benchmark(dir: &str) {
-    println!("dir: {:?}", dir);
-    let benchmark_data = preprocess::FileInputIterator::open(dir).unwrap();
-    let result = benchmark_msm("benchmarks/gpu_explorations", benchmark_data, 1);
-    println!("Done running benchmark: {:?}", result);
+pub fn run_benchmark(
+    instance_size: u32,
+    num_instance: u32,
+    utils_dir: &str,
+    benchmark_dir: &str,
+) -> Result<BenchmarkResult, preprocess::HarnessError> {
+    let benchmark_data_dir = format!("{}/vectors/{}x{}", &utils_dir, instance_size, num_instance);
+
+    // Check if the vectors have been generated
+    match preprocess::FileInputIterator::open(&benchmark_data_dir) {
+        Ok(_) => {
+            println!("Vectors already generated");
+        }
+        Err(_) => {
+            preprocess::gen_vectors(instance_size, num_instance, &benchmark_data_dir);
+        }
+    }
+
+    let benchmark_data = preprocess::FileInputIterator::open(&benchmark_data_dir).unwrap();
+    let instance_durations = benchmark_msm(benchmark_dir, benchmark_data, 1).unwrap();
+    // in milliseconds
+    let avg_processing_time: f64 = instance_durations
+        .iter()
+        .map(|d| d.as_secs_f64() * 1000.0)
+        .sum::<f64>()
+        / instance_durations.len() as f64;
+
+    println!(
+        "Done running benchmark. Check the result at: mopro-core/{:?}",
+        benchmark_dir
+    );
+    Ok(BenchmarkResult {
+        instance_size: instance_size,
+        num_instance: num_instance,
+        avg_processing_time: avg_processing_time,
+    })
 }
-
-// Run the msm benchmark with timing
-// pub fn run_benchmark(dir: &str) -> Result<BenchmarkResult, Box<dyn Error>> {
-//     let num_msm = num_msm.unwrap_or(1000); // default to 1000 msm operations
-
-//     let mut total_msm = Duration::new(0, 0);
-//     for _ in 0..num_msm {
-//         let start = Instant::now();
-//         benchmark_msm()?;
-//         total_msm += start.elapsed();
-//     }
-
-//     let msm_avg = (total_msm.as_secs_f64() / num_msm as f64) * 1_000.0; // in ms
-
-//     Ok(BenchmarkResult {
-//         num_msm,
-//         avg_processing_time: msm_avg,
-//         total_processing_time: total_msm.as_secs_f64() * 1_000.0,
-//     })
-// }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const INSTANCE_SIZE: u32 = 16;
+    const NUM_INSTANCE: u32 = 10;
+    const UTILSPATH: &str = "src/middleware/gpu_explorations/utils";
+    const BENCHMARKSPATH: &str = "benchmarks/gpu_explorations";
+
     #[test]
-    fn test_run_benchmark() {
-        const INSTANCE_SIZE: usize = 16;
-        const NUM_INSTANCES: usize = 10;
-        const PATH: &str = "src/middleware/gpu_explorations/utils";
-        let dir = format!("{}/vectors/{}x{}", PATH, NUM_INSTANCES, INSTANCE_SIZE);
-        run_benchmark(&dir);
+    fn test_benchmark_msm() {
+        let dir = format!("{}/vectors/{}x{}", UTILSPATH, INSTANCE_SIZE, NUM_INSTANCE);
+
+        // Check if the vectors have been generated
+        match preprocess::FileInputIterator::open(&dir) {
+            Ok(_) => {
+                println!("Vectors already generated");
+            }
+            Err(_) => {
+                preprocess::gen_vectors(INSTANCE_SIZE, NUM_INSTANCE, &dir);
+            }
+        }
+
+        let benchmark_data = preprocess::FileInputIterator::open(&dir).unwrap();
+        let result = benchmark_msm(&BENCHMARKSPATH, benchmark_data, 1);
+        println!("Done running benchmark: {:?}", result);
     }
 
-    // #[test]
-    // fn test_single_msm() {
-    //     assert!(benchmark_msm().is_ok());
-    // }
+    #[test]
+    fn test_run_benchmark() {
+        let result =
+            run_benchmark(INSTANCE_SIZE, NUM_INSTANCE, &UTILSPATH, &BENCHMARKSPATH).unwrap();
+        println!("Benchmark result: {:#?}", result);
+    }
 
-    // #[test]
-    // fn test_run_msm_benchmark() {
-    //     let benchmarks = run_msm_benchmark(None).unwrap();
-    //     println!("\nBenchmarking {:?} msm on BN254 curve", benchmarks.num_msm);
-    //     println!(
-    //         "└─ Average msm time: {:.5} ms\n└─ Overall processing time: {:.5} ms",
-    //         benchmarks.avg_processing_time, benchmarks.total_processing_time
-    //     );
-    // }
+    #[test]
+    fn test_run_multi_benchmarks() {
+        let output_path = format!("{}/{}_benchmark.txt", &BENCHMARKSPATH, "arkworks_pippenger");
+        let mut output_file = File::create(output_path).expect("output file creation failed");
+        writeln!(output_file, "msm_size,num_msm,avg_processing_time(ms)");
+        let instance_size = vec![1, 4, 8, 12, 16];
+        let num_instance = vec![1, 5, 10];
+        for size in &instance_size {
+            for num in &num_instance {
+                let result = run_benchmark(*size, *num, &UTILSPATH, &BENCHMARKSPATH).unwrap();
+                println!("{}x{} result: {:#?}", *size, *num, result);
+                writeln!(
+                    output_file,
+                    "{},{},{}",
+                    result.instance_size, result.num_instance, result.avg_processing_time
+                );
+            }
+        }
+    }
 }
