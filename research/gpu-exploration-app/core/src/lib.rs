24679d28476ab8491bb1c74f929a7bd6b5617b29
Jeff Chung <sh1001309@gmail.com>
2024-05-15 13:30:28 +0100

    chore: Create necessary directories for benchmarks

--- research/gpu-exploration-app/core/src/lib.rs
+++ research/gpu-exploration-app/core/src/lib.rs
@@ -5,16 +5,32 @@ mod tests {
     use mopro_core::middleware::gpu_explorations::{
         arkworks_pippenger, trapdoortech_zprize_msm, utils::benchmark::BenchmarkResult, utils::preprocess::HarnessError,
     };
-    use std::fs::File;
+    use std::fs::{create_dir_all, File};
+    use std::path::Path;
 
-    const UTILSPATH: &str = "benchmarks/vectors";
-    const BENCHMARKSPATH: &str = "benchmarks/results";
+    const BENCHMARKS_DIR: &str = "benchmarks";
+    const UTILS_PATH: &str = "benchmarks/vectors";
+    const BENCHMARKS_PATH: &str = "benchmarks/results";
 
+    fn ensure_dir_exists(dir_path: &str) {
+        if !Path::new(dir_path).exists() {
+            create_dir_all(dir_path).expect("Failed to create directory");
+        }
+    }
+
+    fn setup_dirs() {
+        ensure_dir_exists(BENCHMARKS_DIR);
+        ensure_dir_exists(UTILS_PATH);
+        ensure_dir_exists(BENCHMARKS_PATH);
+    }
+    
     fn run_benchmarks<F>(algorithm: &str, instance_size: &[usize], num_instance: &[usize], benchmark_fn: F)
     where
         F: Fn(usize, usize, &str) -> Result<BenchmarkResult, HarnessError> + Sync,
     {
-        let output_path = format!("{}/{}_benchmark.txt", BENCHMARKSPATH, algorithm);
+        setup_dirs(); // to check the directories
+
+        let output_path = format!("{}/{}_benchmark.txt", BENCHMARKS_PATH, algorithm);
         let mut output_file = File::create(output_path).expect("output file creation failed");
         writeln!(output_file, "msm_size,num_msm,avg_processing_time(ms)").unwrap();
 
@@ -24,7 +40,7 @@ mod tests {
                 num_instance
                     .iter()
                     .map(|num| {
-                        let utils_path = format!("{}/{}x{}", UTILSPATH, *size, *num);
+                        let utils_path = format!("{}/{}x{}", UTILS_PATH, *size, *num);
                         let result = benchmark_fn(*size, *num, &utils_path).unwrap();
                         println!("{}x{} result: {:#?}", *size, *num, result);
                         (result.instance_size, result.num_instance, result.avg_processing_time)
