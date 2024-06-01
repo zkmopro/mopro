3a6d79116a260ecd503bbcf7ad6ff23b111438d0
moven0831 <moven0831@gmail.com>
2024-06-01 22:48:19 +0800

    feat(metal): implement arkworks msm accumulation logic in metal

--- mopro-core/src/middleware/gpu_explorations/metal/tests/test_msm_accumulation.rs
+++ mopro-core/src/middleware/gpu_explorations/metal/tests/test_msm_accumulation.rs
@@ -1,16 +1,21 @@
 #[cfg(all(test))]
 mod tests {
-    use ark_bn254::Config;
-    use ark_bn254::{Fq, Fr as ScalarField, G1Affine as GAffine, G1Projective as G};
-    use ark_ec::{AffineRepr, CurveGroup, Group, VariableBaseMSM};
+    use ark_bn254::{Fq, FqConfig, Fr as ScalarField, G1Affine as GAffine, G1Projective as G};
+    use ark_ec::{
+        short_weierstrass::Projective, twisted_edwards::MontCurveConfig, AffineRepr, CurveGroup,
+        Group, VariableBaseMSM,
+    };
     use ark_ff::{
         biginteger::{BigInteger, BigInteger256},
-        BigInt, Field, PrimeField, UniformRand,
+        models::{MontBackend, MontConfig, MontFp},
+        PrimeField, UniformRand,
     };
-    use ark_std::{borrow::Borrow, cfg_into_iter, iterable::Iterable, rand, vec::Vec, One, Zero};
+    use ark_std::{cfg_into_iter, rand, vec::Vec, One, Zero};
 
     use crate::middleware::gpu_explorations::metal::abstraction::{
-        errors::MetalError, state::*, utils::*,
+        errors::MetalError,
+        limbs_conversion::{FromLimbs, ToLimbs},
+        state::*,
     };
 
     use metal::*;
@@ -26,26 +31,28 @@ mod tests {
     }
 
     #[test]
-    fn test_msm_bigint() {
-        let state = MetalState::new(None).unwrap();
-
-        let num_points = 1000;
-        let window_size = 4;
-        let num_bits = ScalarField::MODULUS_BIT_SIZE as usize;
-        let num_scalars = 1000;
+    fn test_msm_accumulation_phase() {
+        /* Init params for both schemes */
+        let num_points = 2;
+        let num_scalars = 2;
+        let modulus_bit_size = ScalarField::MODULUS_BIT_SIZE as usize;
+        let one = ScalarField::one().into_bigint();
+        let zero = G::zero();
 
         let mut rng = rand::thread_rng();
-        let points: Vec<GAffine> = (0..num_points)
-            .map(|_| G::rand(&mut rng).into_affine())
-            .collect();
-        let scalars: Vec<ScalarField> = (0..num_scalars)
-            .map(|_| ScalarField::rand(&mut rng))
-            .collect();
-
-        let bigints = cfg_into_iter!(scalars.clone())
-            .map(|s| s.into_bigint())
-            .collect::<Vec<_>>();
-        let instances_size = ark_std::cmp::min(points.len(), bigints.len());
+        // let points: Vec<GAffine> = (0..num_points)
+        //     .map(|_| G::rand(&mut rng).into_affine())
+        //     .collect();
+        // let scalars: Vec<ScalarField> = (0..num_scalars)
+        // .map(|_| ScalarField::rand(&mut rng))
+        // .collect();
+        let a_point = G::rand(&mut rng).into_affine();
+        let b_point = G::rand(&mut rng).into_affine();
+        let s1 = ScalarField::from(2u64);
+        let s2 = ScalarField::from(3u64);
+        let points: Vec<GAffine> = vec![a_point.clone(), b_point.clone()];
+        let scalars: Vec<ScalarField> = vec![s1.clone(), s2.clone()];
+        let instances_size = ark_std::cmp::min(points.len(), scalars.len());
         let c = if instances_size < 32 {
             3
         } else {
@@ -53,18 +60,8 @@ mod tests {
         };
         let buckets_size = (1 << c) - 1;
 
-        let scalars_arkworks = &bigints[..instances_size];
         let bases = &points[..instances_size];
-        let scalars_and_bases_iter = scalars_arkworks
-            .iter()
-            .zip(bases)
-            .filter(|(s, _)| !s.is_zero());
-
-        let num_bits = ScalarField::MODULUS_BIT_SIZE as usize;
-        let one = ScalarField::one().into_bigint();
-
-        let zero = G::zero();
-        let window_starts: Vec<_> = (0..num_bits).step_by(c).collect();
+        let window_starts: Vec<_> = (0..modulus_bit_size).step_by(c).collect();
 
         /* do msm accumulation stage on metal */
         // flatten scalar and base to Vec<u32>
@@ -72,6 +69,11 @@ mod tests {
             .map(|s| s.0.to_u32_limbs())
             .flatten()
             .collect::<Vec<u32>>();
+
+        // The base point should be in montgomery form
+        // montgomery form point at infinity: x = 0, y = 1, z = 0
+        // sw form point at infinity: x = 1, y = 1, z = 0
+
         let bases_limbs = cfg_into_iter!(bases)
             .map(|b| {
                 let b = b.into_group();
@@ -100,9 +102,14 @@ mod tests {
         };
 
         // store params to GPU shared memory
+        let state = MetalState::new(None).unwrap();
         let window_size_buffer = state.alloc_buffer_data(&[c as u32]);
         let scalar_buffer = state.alloc_buffer_data(&scalars_limbs);
         let base_buffer = state.alloc_buffer_data(&bases_limbs);
+
+        // Debug buffer
+        let debug_buffer_k = state.alloc_buffer::<u32>(8);
+        let debug_buffer_p = state.alloc_buffer::<u32>(8);
 
         let calc_bucket_pipe = state.setup_pipeline("calculate_buckets").unwrap();
 
@@ -110,7 +117,7 @@ mod tests {
             .map(|w_start| {
                 // Get the res value
                 let mut res = zero;
-                if w_start == 0 || scalars[0].0 == one {
+                if w_start == 0 && scalars[0].0 == one {
                     res += bases[0];
                 }
 
@@ -124,6 +131,8 @@ mod tests {
                             (2, &scalar_buffer),
                             (3, &base_buffer),
                             (4, &buckets_buffer),
+                            (5, &debug_buffer_k),
+                            (6, &debug_buffer_p),
                         ]),
                     );
 
@@ -138,19 +147,32 @@ mod tests {
                     command_buffer.wait_until_completed();
                 });
 
+                // Debug buffer
+                let debug_k = MetalState::retrieve_contents::<u32>(&debug_buffer_k);
+                let debug_p = MetalState::retrieve_contents::<u32>(&debug_buffer_p);
+                println!("(metal) debug_k: {:?}", debug_k);
+                println!("(metal) debug_p: {:?}", debug_p);
+
+                // println!("(metal) origin_k: {:#?}", scalars_limbs);
+                // println!("(metal) origin_p: {:#?}", bases_limbs);
+
                 // recover the points from the buckets
                 let buckets = {
                     let raw_limbs = MetalState::retrieve_contents::<u32>(&buckets_buffer);
+                    println!("(metal) raw_limbs: {:?}", raw_limbs);
                     let limbs = raw_limbs
                         .chunks(8)
                         .map(|x| BigInteger256::from_u32_limbs(&x))
                         .collect::<Vec<_>>();
+                    println!("(metal) limbs: {:?}", limbs);
                     limbs
                         .chunks(3)
                         .map(|chunk| {
                             let x = <Fq as PrimeField>::from_bigint(chunk[0]).unwrap();
                             let y = <Fq as PrimeField>::from_bigint(chunk[1]).unwrap();
                             let z = <Fq as PrimeField>::from_bigint(chunk[2]).unwrap();
+                            println!("(metal) (x, y, z): {:?}", (x, y, z));
+                            // println!("(metal) new point: {:?}", G::new(x, y, z).is_zero());
                             G::new_unchecked(x, y, z)
                         })
                         .collect::<Vec<_>>()
@@ -160,11 +182,20 @@ mod tests {
             })
             .collect();
         println!(
-            "(metal) metal_accumulation_phase: {:?}",
+            "(metal) metal_accumulation_phase: {:#?}\n",
             metal_accumulation_phase
         );
 
-        /* do msm accumulation stage on arkworks*/
+        /* do msm accumulation stage on arkworks */
+        let bigints = cfg_into_iter!(scalars.clone())
+            .map(|s| s.into_bigint())
+            .collect::<Vec<_>>();
+        let scalars_arkworks = &bigints[..instances_size];
+        let scalars_and_bases_iter = scalars_arkworks
+            .iter()
+            .zip(bases)
+            .filter(|(s, _)| !s.is_zero());
+
         let arkworks_accumulation_phase: Vec<_> = ark_std::cfg_into_iter!(window_starts)
             .map(|w_start| {
                 let mut res = zero;
@@ -206,7 +237,7 @@ mod tests {
             })
             .collect();
         println!(
-            "(arkworks) arkworks_accumulation_phase: {:?}",
+            "(arkworks) arkworks_accumulation_phase: {:#?}\n",
             arkworks_accumulation_phase
         );
 
@@ -261,7 +292,14 @@ mod tests {
                     total
                 });
 
-        let msm = <G as VariableBaseMSM>::msm(&points, &scalars).unwrap();
+        let msm = G::msm(&[a_point, b_point], &[s1, s2]).unwrap();
         assert_eq!(arkworks_result, msm);
+
+        let small_msm = a_point * s1 + b_point * s2;
+        println!("(arkworks) arkworks_result: {:?}", arkworks_result);
+        println!("(arkworks) msm: {:?}", msm);
+        println!("(arkworks) small_msm: {:?}", small_msm);
+        assert_eq!(arkworks_result, small_msm);
+        assert_eq!(msm, small_msm);
     }
 }
