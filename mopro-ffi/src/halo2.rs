844eb65f8c84d3c9d1c0481bb53afbfe3e3ef3b6
Artem <48178500+ElusAegis@users.noreply.github.com>
2024-06-02 11:46:27 +0200

    (feat: ffi) updated ffi to be compatible with core changes

--- mopro-ffi/src/halo2.rs
+++ mopro-ffi/src/halo2.rs
@@ -1,22 +1,21 @@
 #![ allow(unused_variables)]
 
-#[cfg(feature = "halo2")]
-use std::str::FromStr;
-use mopro_core::MoproError;
-use crate::{GenerateProofResult};
+use std::collections::HashMap;
 
 #[cfg(feature = "halo2")]
 use mopro_core::middleware::halo2;
+use mopro_core::middleware::halo2::deserialize_circuit_inputs;
+use mopro_core::MoproError;
 
+use crate::GenerateProofResult;
 
 #[cfg(feature = "halo2")]
 pub fn generate_halo2_proof2(
-    input: String
+    circuit_inputs: HashMap<String, Vec<String>>
 ) -> Result<GenerateProofResult, MoproError> {
-    // Convert input to u64
-    let bigint_inputs: u64 = u64::from_str(&input).unwrap();
+    let circuit_inputs = deserialize_circuit_inputs(circuit_inputs);
 
-    let (proof, inputs) = halo2::generate_halo2_proof2(bigint_inputs).unwrap();
+    let (proof, inputs) = halo2::generate_halo2_proof2(circuit_inputs).unwrap();
 
 
     let serialized_proof = bincode::serialize(&proof).map_err(|e| MoproError::Halo2Error(e.to_string()))?;
@@ -30,16 +29,16 @@ pub fn generate_halo2_proof2(
 
 #[cfg(not(feature = "halo2"))]
 pub fn generate_halo2_proof2(
-    input: String
+    inputs: HashMap<String, Vec<String>>
 ) -> Result<GenerateProofResult, MoproError> {
         Err(MoproError::Halo2Error("Project is compiled for Circom proving system. This function is currently not supported in Circom.".to_string()))
 }
 
 
 #[cfg(feature = "halo2")]
-pub fn verify_halo2_proof2(proof: Vec<u8>, inputs: Vec<u8>) -> Result<bool, MoproError> {
+pub fn verify_halo2_proof2(proof: Vec<u8>, public_inputs: Vec<u8>) -> Result<bool, MoproError> {
     let deserialized_proof: halo2::SerializableProof = bincode::deserialize(&proof).map_err(|e| MoproError::Halo2Error(e.to_string()))?;
-    let deserialized_inputs: halo2::SerializableInputs = bincode::deserialize(&inputs).map_err(|e| MoproError::Halo2Error(e.to_string()))?;
+    let deserialized_inputs: halo2::SerializablePublicInputs = bincode::deserialize(&public_inputs).map_err(|e| MoproError::Halo2Error(e.to_string()))?;
     let is_valid = halo2::verify_halo2_proof2(deserialized_proof, deserialized_inputs).unwrap();
     Ok(is_valid)
 }
