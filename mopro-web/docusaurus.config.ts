7b172091645646456262922b66211dafbb2be9e5
CJ Rose <cjrose59@gmail.com>
2024-06-05 15:05:00 -0700

    remove comments

--- mopro-web/docusaurus.config.ts
+++ mopro-web/docusaurus.config.ts
@@ -1,29 +1,16 @@
-// import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'Mopro',
   tagline: 'Making client-side proving on mobile simple.',
-  favicon: 'img/logo_sm.svg',
-
-  // Set the production url of your site here
+  favicon: '/img/logo_sm.svg',
   url: 'https://zkmopro.org',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'zkmopro', // Usually your GitHub org/user name.
-  projectName: 'mopro', // Usually your repo name.
-
+  organizationName: 'zkmopro',
+  projectName: 'mopro',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -56,11 +43,9 @@ const config: Config = {
       disableSwitch: true,
       respectPrefersColorScheme: false,
     },
-    // Replace with your project's social card
     image: 'img/logo_title.svg',
     navbar: {
       style: 'dark',
-      // title: 'Mopro',
       logo: {
         alt: 'Mopro Logo',
         src: 'img/logo_title.svg',
@@ -135,10 +120,6 @@ const config: Config = {
       },
       copyright: `Copyright © ${new Date().getFullYear()} Mopro`,
     },
-    // prism: {
-    //   theme: prismThemes.github,
-    //   darkTheme: prismThemes.dracula,
-    // },
   } satisfies Preset.ThemeConfig,
 };
 
