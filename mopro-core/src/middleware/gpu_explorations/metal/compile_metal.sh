9c4ae451190d54ee05e6b9c3403da4d90b8f9be9
Jeff Chung <sh1001309@gmail.com>
2024-06-11 15:40:09 +0100

    feat: update library compilation cmd and config for diff target

--- mopro-core/src/middleware/gpu_explorations/metal/compile_metal.sh
+++ mopro-core/src/middleware/gpu_explorations/metal/compile_metal.sh
@@ -1,9 +1,24 @@
 # every kernel is self contained i.e. is its own crate. Simply `cd` into a kernel's directory and run
 # the following, which compiles our shader to an intermediate representation using the metal utility
-xcrun -sdk macosx metal -c ./mopro-core/src/middleware/gpu_explorations/metal/shader/all.metal -o ./mopro-core/src/middleware/gpu_explorations/metal/shader/all.air
+
+# input a cmd parameter that is able to change the target of macosx and iphoneos
+# ios: compileing metal with `iphoneos` target
+# mac: compiling metal with `macosx` target
+ARG=$1
+if [ $ARG == "ios" ]; then
+    TARGET="iphoneos"
+elif [ $ARG == "mac" ]; then
+    TARGET="macosx"
+else
+    echo "Unknown target: $ARG"
+    echo "Please enter either 'ios' or 'mac' as parameter to compile"
+    exit 1
+fi
+
+xcrun -sdk $TARGET metal -c ./mopro-core/src/middleware/gpu_explorations/metal/shader/all.metal -o ./mopro-core/src/middleware/gpu_explorations/metal/shader/all.air
 
 # next, compile the .air file to generate a .metallib file - which I believe is LLVM IR (need confirmation)
-xcrun -sdk macosx metallib ./mopro-core/src/middleware/gpu_explorations/metal/shader/all.air -o ./mopro-core/src/middleware/gpu_explorations/metal/shader/msm.metallib
+xcrun -sdk $TARGET metallib ./mopro-core/src/middleware/gpu_explorations/metal/shader/all.air -o ./mopro-core/src/middleware/gpu_explorations/metal/shader/msm.metallib
 
 # finally, clean the redundant .air file
 rm -f ./mopro-core/src/middleware/gpu_explorations/metal/shader/all.air
