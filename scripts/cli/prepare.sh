0869aa86363a90f91f2142d1e3604e83648262e2
Artem <48178500+ElusAegis@users.noreply.github.com>
2024-06-19 17:43:41 +0100

    (feat: scripts) Halo2 Prepare script now checks if the keys already exists. If they do not exist, there is an explicit warning.

--- scripts/cli/prepare.sh
+++ scripts/cli/prepare.sh
@@ -170,10 +170,34 @@ halo2_generate_keys() {
     exit 1
   fi
 
-  cd $CIRCUIT_DIR
+  cd "$CIRCUIT_DIR" || exit
 
-  # Generate the keys running the cargo command on the circuit binary
-  cargo run --release --bin $CIRCUIT_NAME
+  local circuit_srs_path="out/${CIRCUIT_NAME}_srs"
+  local circuit_pk_path="out/${CIRCUIT_NAME}_pk"
+  local circuit_vk_path="out/${CIRCUIT_NAME}_vk"
+
+  # Check if all keys already exist
+  if [ -f "$circuit_srs_path" ] && [ -f "$circuit_pk_path" ] && [ -f "$circuit_vk_path" ]; then
+    echo "Keys for the circuit $CIRCUIT_NAME already exist."
+    echo "Skipping the key generation step."
+    return
+  fi
+
+  # Check if the circuit binary exists
+  # Execute the cargo run command to generate the keys for the circuit
+  # Handle errors if the cargo command fails
+  if ! cargo run --release --bin "$CIRCUIT_NAME"; then
+      echo "Error: Failed to generate keys using the circuit binary '$CIRCUIT_NAME'."
+      echo "This step is necessary to generate the keys for the circuit."
+      echo "Consider adding the necessary code to the circuit binary to generate the keys as in the example"
+      echo "or generate the keys manually and skip the `prepare` step."
+      echo "Make sure there are the following key files:"
+      echo "  - ${CIRCUIT_DIR}/${circuit_srs_path})"
+      echo "  - ${CIRCUIT_DIR}/${circuit_pk_path})"
+      echo "  - ${CIRCUIT_DIR}/${circuit_vk_path})"
+
+      exit 1
+  fi
 }
 
 
