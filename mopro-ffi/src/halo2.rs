8244012d552bf86b71909656b02e7cc708aa9918
Artem <48178500+ElusAegis@users.noreply.github.com>
2024-06-21 14:29:31 +0100

    (feat: ffi) reworked structure for the `ffi` to have a `circom` feature and a clear separation between different adapters.
    Also renamed the `generate_halo2_proof` to `generate_proof_static` as well as same for verify.

--- mopro-ffi/src/halo2.rs
+++ mopro-ffi/src/halo2.rs
@@ -1,53 +1,106 @@
 #![allow(unused_variables)]
 
-use std::collections::HashMap;
-
 #[cfg(feature = "halo2")]
-use mopro_core::middleware::halo2;
+pub(crate) use common::*;
+
+/// Module that contains all the shared adapter functionality implemented for the Halo2 adapter.
+/// As the adapter is only used when the `halo2` feature is enabled,
+/// we make the compiler avoid the shared functions of module when the feature is not enabled.
 #[cfg(feature = "halo2")]
-use mopro_core::middleware::halo2::deserialize_circuit_inputs;
-use mopro_core::MoproError;
+mod common {
+    use std::collections::HashMap;
 
-use crate::GenerateProofResult;
+    use mopro_core::middleware::halo2;
+    use mopro_core::middleware::halo2::deserialize_circuit_inputs;
+    use mopro_core::MoproError;
 
-#[cfg(feature = "halo2")]
-pub fn generate_halo2_proof(
-    circuit_inputs: HashMap<String, Vec<String>>,
-) -> Result<GenerateProofResult, MoproError> {
-    let circuit_inputs = deserialize_circuit_inputs(circuit_inputs);
+    use crate::GenerateProofResult;
 
-    let (proof, inputs) = halo2::generate_halo2_proof(circuit_inputs).unwrap();
+    pub fn generate_proof_static(
+        circuit_inputs: HashMap<String, Vec<String>>,
+    ) -> Result<GenerateProofResult, MoproError> {
+        let circuit_inputs = deserialize_circuit_inputs(circuit_inputs);
 
-    let serialized_proof =
-        bincode::serialize(&proof).map_err(|e| MoproError::Halo2Error(e.to_string()))?;
-    let serialized_inputs = bincode::serialize(&inputs).expect("Serialization of Inputs failed");
+        let (proof, inputs) = halo2::generate_halo2_proof(circuit_inputs).unwrap();
 
-    Ok(GenerateProofResult {
-        proof: serialized_proof,
-        inputs: serialized_inputs,
-    })
-}
+        let serialized_proof =
+            bincode::serialize(&proof).map_err(|e| MoproError::Halo2Error(e.to_string()))?;
+        let serialized_inputs =
+            bincode::serialize(&inputs).expect("Serialization of Inputs failed");
 
-#[cfg(not(feature = "halo2"))]
-pub fn generate_halo2_proof(
-    inputs: HashMap<String, Vec<String>>,
-) -> Result<GenerateProofResult, MoproError> {
-    Err(MoproError::Halo2Error("Project is compiled for Circom proving system. This function is currently not supported in Circom.".to_string()))
-}
+        Ok(GenerateProofResult {
+            proof: serialized_proof,
+            inputs: serialized_inputs,
+        })
+    }
 
-#[cfg(feature = "halo2")]
-pub fn verify_halo2_proof(proof: Vec<u8>, public_inputs: Vec<u8>) -> Result<bool, MoproError> {
-    let deserialized_proof: halo2::SerializableProof =
-        bincode::deserialize(&proof).map_err(|e| MoproError::Halo2Error(e.to_string()))?;
-    let deserialized_inputs: halo2::SerializablePublicInputs =
-        bincode::deserialize(&public_inputs).map_err(|e| MoproError::Halo2Error(e.to_string()))?;
-    let is_valid = halo2::verify_halo2_proof(deserialized_proof, deserialized_inputs).unwrap();
-    Ok(is_valid)
-}
+    pub fn verify_proof_static(proof: Vec<u8>, public_inputs: Vec<u8>) -> Result<bool, MoproError> {
+        let deserialized_proof: halo2::SerializableProof =
+            bincode::deserialize(&proof).map_err(|e| MoproError::Halo2Error(e.to_string()))?;
+        let deserialized_inputs: halo2::SerializablePublicInputs =
+            bincode::deserialize(&public_inputs)
+                .map_err(|e| MoproError::Halo2Error(e.to_string()))?;
+        let is_valid = halo2::verify_halo2_proof(deserialized_proof, deserialized_inputs).unwrap();
+        Ok(is_valid)
+    }
 
-#[cfg(not(feature = "halo2"))]
-pub fn verify_halo2_proof(proof: Vec<u8>, inputs: Vec<u8>) -> Result<bool, MoproError> {
-    Err(MoproError::Halo2Error(
-        "Project does not have Halo2 feature enabled".to_string(),
-    ))
+    pub fn initialize_mopro() -> Result<(), MoproError> {
+        panic!("Mopro Halo2 does not implement initialization yet.");
+    }
+
+    pub fn initialize_mopro_dylib(dylib_path: String) -> Result<(), MoproError> {
+        panic!("Mopro Halo2 does not implement dylib initialization yet.");
+    }
+
+    #[cfg(test)]
+    #[test]
+    #[cfg(feature = "circom")]
+    fn test_end_to_end() -> Result<(), MoproError> {
+        // Paths to your wasm and zkey files
+        let wasm_path =
+            "./../mopro-core/examples/circom/multiplier2/target/multiplier2_js/multiplier2.wasm";
+        let zkey_path = "./../mopro-core/examples/circom/multiplier2/target/multiplier2_final.zkey";
+
+        // Create a new MoproCircom instance
+        let mopro_circom = MoproCircom::new();
+
+        // Step 1: Initialize
+        let init_result = mopro_circom.initialize(zkey_path.to_string(), wasm_path.to_string());
+        assert!(init_result.is_ok());
+
+        let mut inputs = HashMap::new();
+        let a = BigUint::from_str(
+            "21888242871839275222246405745257275088548364400416034343698204186575808495616",
+        )
+        .unwrap();
+        let b = BigUint::from(1u8);
+        let c = a.clone() * b.clone();
+        inputs.insert("a".to_string(), vec![a.to_string()]);
+        inputs.insert("b".to_string(), vec![b.to_string()]);
+        // output = [public output c, public input a]
+        let expected_output = vec![Fr::from(c), Fr::from(a)];
+        let circom_outputs = circom::serialization::SerializableInputs(expected_output);
+        let serialized_outputs = circom::serialization::serialize_inputs(&circom_outputs);
+
+        // Step 2: Generate Proof
+        let generate_proof_result = mopro_circom.generate_proof(inputs)?;
+        let serialized_proof = generate_proof_result.proof;
+        let serialized_inputs = generate_proof_result.inputs;
+
+        assert!(serialized_proof.len() > 0);
+        assert_eq!(serialized_inputs, serialized_outputs);
+
+        // Step 3: Verify Proof
+        let is_valid =
+            mopro_circom.verify_proof(serialized_proof.clone(), serialized_inputs.clone())?;
+        assert!(is_valid);
+
+        // Step 4: Convert Proof to Ethereum compatible proof
+        let proof_calldata = crate::adapter::to_ethereum_proof(serialized_proof);
+        let inputs_calldata = crate::adapter::to_ethereum_inputs(serialized_inputs);
+        assert!(proof_calldata.a.x.len() > 0);
+        assert!(inputs_calldata.len() > 0);
+
+        Ok(())
+    }
 }
