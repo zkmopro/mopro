f0b3601deea27623c0114bd9c76922b5c81ffac8
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-06-24 21:43:26 +0800

    chore: remove deprecated folders

--- scripts/prepare_ci.sh
+++ scripts/prepare_ci.sh
@@ -4,8 +4,8 @@
 source "scripts/_prelude.sh"
 
 # Assert we're in the project root
-if [[ ! -d "mopro-ffi" || ! -d "mopro-core" || ! -d "mopro-ios" ]]; then
-    echo -e "${RED}Error: This script must be run from the project root directory that contains mopro-ffi, mopro-core, and mopro-ios folders.${DEFAULT}"
+if [[ ! -d "mopro-ffi" || ! -d "mopro-core" ]]; then
+    echo -e "${RED}Error: This script must be run from the project root directory that contains mopro-ffi and mopro-core folders.${DEFAULT}"
     exit 1
 fi
 
