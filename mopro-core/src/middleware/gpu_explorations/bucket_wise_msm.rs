f41d8b94de7b1175502999f3c0e285391903bb24
Jeff Chung <sh1001309@gmail.com>
2024-06-26 21:31:40 +0100

    chore: delete msm bigint

--- mopro-core/src/middleware/gpu_explorations/bucket_wise_msm.rs
+++ mopro-core/src/middleware/gpu_explorations/bucket_wise_msm.rs
@@ -19,107 +19,6 @@ fn ln_without_floats(a: usize) -> usize {
     (ark_std::log2(a) * 69 / 100) as usize
 }
 
-// /// Optimized implementation of multi-scalar multiplication.
-// fn msm_bigint<V: VariableBaseMSM>(
-//     bases: &[V::MulBase],
-//     scalars: &[V::ScalarField],
-// ) -> V {
-//     let bigints = cfg_into_iter!(scalars)
-//             .map(|s| s.into_bigint())
-//             .collect::<Vec<_>>();
-
-//     let size = ark_std::cmp::min(bases.len(), bigints.len());
-//     let scalars = &bigints[..size];
-//     let bases = &bases[..size];
-//     let scalars_and_bases_iter = scalars.iter().zip(bases).filter(|(s, _)| !s.is_zero());
-
-//     let c = if size < 32 {
-//         3
-//     } else {
-//         ln_without_floats(size) + 2
-//     };
-
-//     let num_bits = V::ScalarField::MODULUS_BIT_SIZE as usize;
-//     let one = V::ScalarField::one().into_bigint();
-
-//     let zero = V::zero();
-//     let window_starts: Vec<_> = (0..num_bits).step_by(c).collect();
-
-//     // Each window is of size `c`.
-//     // We divide up the bits 0..num_bits into windows of size `c`, and
-//     // in parallel process each such window.
-//     let window_sums: Vec<_> = ark_std::cfg_into_iter!(window_starts)
-//         .map(|w_start| {
-//             let mut res = zero;
-//             // We don't need the "zero" bucket, so we only have 2^c - 1 buckets.
-//             let mut buckets = vec![zero; (1 << c) - 1];
-//             // This clone is cheap, because the iterator contains just a
-//             // pointer and an index into the original vectors.
-//             scalars_and_bases_iter.clone().for_each(|(&scalar, base)| {
-//                 if scalar == one {
-//                     // We only process unit scalars once in the first window.
-//                     if w_start == 0 {
-//                         res += base;
-//                     }
-//                 } else {
-//                     let mut scalar = scalar;
-
-//                     // We right-shift by w_start, thus getting rid of the
-//                     // lower bits.
-//                     scalar.divn(w_start as u32);
-
-//                     // We mod the remaining bits by 2^{window size}, thus taking `c` bits.
-//                     let scalar = scalar.as_ref()[0] % (1 << c);
-
-//                     // If the scalar is non-zero, we update the corresponding
-//                     // bucket.
-//                     // (Recall that `buckets` doesn't have a zero bucket.)
-//                     if scalar != 0 {
-//                         buckets[(scalar - 1) as usize] += base;
-//                     }
-//                 }
-//             });
-
-//             // Compute sum_{i in 0..num_buckets} (sum_{j in i..num_buckets} bucket[j])
-//             // This is computed below for b buckets, using 2b curve additions.
-//             //
-//             // We could first normalize `buckets` and then use mixed-addition
-//             // here, but that's slower for the kinds of groups we care about
-//             // (Short Weierstrass curves and Twisted Edwards curves).
-//             // In the case of Short Weierstrass curves,
-//             // mixed addition saves ~4 field multiplications per addition.
-//             // However normalization (with the inversion batched) takes ~6
-//             // field multiplications per element,
-//             // hence batch normalization is a slowdown.
-
-//             // `running_sum` = sum_{j in i..num_buckets} bucket[j],
-//             // where we iterate backward from i = num_buckets to 0.
-//             let mut running_sum = V::zero();
-//             buckets.into_iter().rev().for_each(|b| {
-//                 running_sum += &b;
-//                 res += &running_sum;
-//             });
-//             res
-//         })
-//         .collect();
-
-//     // We store the sum for the lowest window.
-//     let lowest = *window_sums.first().unwrap();
-
-//     // We're traversing windows from high to low.
-//     lowest
-//         + &window_sums[1..]
-//             .iter()
-//             .rev()
-//             .fold(zero, |mut total, sum_i| {
-//                 total += sum_i;
-//                 for _ in 0..c {
-//                     total.double_in_place();
-//                 }
-//                 total
-//             })
-// }
-
 /// use bucket-wise accumulation in msm
 fn bucket_wise_msm<V: VariableBaseMSM>(
     bases: &[V::MulBase],
@@ -300,9 +199,7 @@ where
         let mut instance_total_duration = Duration::ZERO;
         for _i in 0..iterations {
             let start = Instant::now();
-            let _result = bucket_wise_msm::<G>(&points[..], &scalars[..]).unwrap();
-            // let _result = msm_bigint::<G>(&points[..], &scalars[..]);
-
+            let result = bucket_wise_msm::<G>(&points[..], &scalars[..]).unwrap();
             instance_total_duration += start.elapsed();
         }
         let instance_avg_duration = instance_total_duration / iterations;
