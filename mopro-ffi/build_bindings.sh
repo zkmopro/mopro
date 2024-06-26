b45befcaf545cb44ac5739362e45f93281bf99c3
Chance <jchancehud@gmail.com>
2024-06-26 12:42:22 -0700

    fix: platform parsing

--- mopro-ffi/build_bindings.sh
+++ mopro-ffi/build_bindings.sh
@@ -2,4 +2,7 @@
 
 set -e
 
+# This script only needs to be run when we change the UDL file
+# The resulting output should be committed to the repo
+
 cargo run --bin uniffi-bindgen generate src/mopro.udl --language swift --out-dir SwiftBindings
