5105bb334350ef718ae72d44205fe73096eddedc
vimwitch <jchancehud@gmail.com>
2024-07-06 14:58:50 -0700

    fix: only link test witness fn when building locally

--- mopro-ffi/build.rs
+++ mopro-ffi/build.rs
@@ -1,5 +1,6 @@
 fn main() {
-    if cfg!(test) {
+    let link_test_witness = std::env::var("MOPRO_FFI_LINK_TEST_WITNESS").unwrap_or("".to_string());
+    if link_test_witness != "" {
         rust_witness::transpile::transpile_wasm("../test-vectors/circom".to_string());
     }
 }
