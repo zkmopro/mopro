e196f45678ad169f37706e0747b5856ff69cd0fa
vimwitch <jchancehud@gmail.com>
2024-07-07 15:17:27 -0700

    refactor: cleanup env var expr

--- mopro-ffi/build.rs
+++ mopro-ffi/build.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let link_test_witness = std::env::var("MOPRO_FFI_LINK_TEST_WITNESS").unwrap_or("".to_string());
-    if link_test_witness != "" {
+    if std::env::var("MOPRO_FFI_LINK_TEST_WITNESS").unwrap_or_default() != "" {
         rust_witness::transpile::transpile_wasm("../test-vectors/circom".to_string());
     }
 }
