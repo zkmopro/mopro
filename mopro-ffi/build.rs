2e3c38253963656a1883f32e5d7da3e58ff94188
Chance <jchancehud@gmail.com>
2024-07-07 23:14:05 -0700

    Test witness linking (#206)
    * fix: only link test witness fn when building locally
    
    * chore: set env vars where needed
    
    * fix: duplicate library warning
    
    * refactor: cleanup env var expr

--- mopro-ffi/build.rs
+++ mopro-ffi/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if cfg!(test) {
+    if std::env::var("MOPRO_FFI_LINK_TEST_WITNESS").unwrap_or_default() != "" {
         rust_witness::transpile::transpile_wasm("../test-vectors/circom".to_string());
     }
 }
