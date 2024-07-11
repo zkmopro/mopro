4b2fede93a5530f31fe4632f403a3af0d44a54da
Artem <48178500+ElusAegis@users.noreply.github.com>
2024-07-11 15:54:19 +0100

    (feat: halo2) added a test case for Halo2

--- test-e2e/tests/test_generated_bindings.rs
+++ test-e2e/tests/test_generated_bindings.rs
@@ -1,3 +1,4 @@
+#[cfg(feature = "circom")]
 uniffi::build_foreign_language_testcases!(
     "tests/bindings/test_circom_multiplier2.swift",
     "tests/bindings/test_circom_multiplier2.kts",
@@ -5,4 +6,10 @@ uniffi::build_foreign_language_testcases!(
     "tests/bindings/test_circom_keccak.kts",
     "tests/bindings/test_circom_multiplier2_bls.swift",
     "tests/bindings/test_circom_multiplier2_bls.kts",
+);
+
+#[cfg(feature = "halo2")]
+uniffi::build_foreign_language_testcases!(
+    "tests/bindings/test_halo2_fibonacci.swift",
+    "tests/bindings/test_halo2_fibonacci.kts",
 );
