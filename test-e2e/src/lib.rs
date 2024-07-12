06d1e1621c1dab73c6f3e6495c1578c89b6e39c7
Artem <48178500+ElusAegis@users.noreply.github.com>
2024-07-12 10:04:56 +0100

    (feat: test-e2e) Removed feature conditional compilation from the `test-e2e` crate. As a solution for confitional tests, moved different `adapter` tests into different targets.

--- test-e2e/src/lib.rs
+++ test-e2e/src/lib.rs
@@ -3,48 +3,27 @@ extern crate core;
 // First, configure the Mopro FFI library
 mopro_ffi::app!();
 
-/// Macro containing the Circom example
-#[cfg(feature = "circom")]
-macro_rules! circom_example {
-    () => {
-        // Circom Example of setting up 4 circuits
-        rust_witness::witness!(multiplier2);
-        rust_witness::witness!(multiplier2bls);
-        rust_witness::witness!(keccak256256test);
-        rust_witness::witness!(hashbenchbls);
+// Circom Example of setting up 4 circuits
+rust_witness::witness!(multiplier2);
+rust_witness::witness!(multiplier2bls);
+rust_witness::witness!(keccak256256test);
+rust_witness::witness!(hashbenchbls);
 
-        mopro_ffi::set_circom_circuits!(
-            "multiplier2_final.zkey",
-            multiplier2_witness,
-            "multiplier2_bls_final.zkey",
-            multiplier2bls_witness,
-            "keccak256_256_test_final.zkey",
-            keccak256256test_witness,
-        );
-        // Circom Snipet End
-    };
-}
+mopro_ffi::set_circom_circuits!(
+    "multiplier2_final.zkey",
+    multiplier2_witness,
+    "multiplier2_bls_final.zkey",
+    multiplier2bls_witness,
+    "keccak256_256_test_final.zkey",
+    keccak256256test_witness,
+);
+// Circom Snipet End
 
-/// Macro containing the Halo2 example
-#[cfg(feature = "halo2")]
-macro_rules! halo2_example {
-    () => {
-        // Halo2 Sample of using a single proving and verifying circuit
+// Halo2 Sample of using a single proving and verifying circuit
 
-        // Module containing the Halo2 circuit logic (FibonacciMoproCircuit)
-        mod halo2;
+// Module containing the Halo2 circuit logic (FibonacciMoproCircuit)
+mod halo2;
 
-        mopro_ffi::set_halo2_proving_circuits!("fibonacci_pk", halo2::FibonacciMoproCircuit::prove);
-        mopro_ffi::set_halo2_verifying_circuits!(
-            "fibonacci_vk",
-            halo2::FibonacciMoproCircuit::verify
-        );
-        // Halo2 Snipet End
-    };
-}
-
-// Based on the feature flags, include the respective examples
-#[cfg(feature = "circom")]
-circom_example!();
-#[cfg(feature = "halo2")]
-halo2_example!();
+mopro_ffi::set_halo2_proving_circuits!("fibonacci_pk", halo2::FibonacciMoproCircuit::prove);
+mopro_ffi::set_halo2_verifying_circuits!("fibonacci_vk", halo2::FibonacciMoproCircuit::verify);
+// Halo2 Snipet End
---
+++ test-e2e/tests/halo2.rs
@@ -0,0 +1,4 @@
+uniffi::build_foreign_language_testcases!(
+    "tests/bindings/test_halo2_fibonacci.swift",
+    "tests/bindings/test_halo2_fibonacci.kts",
+);
