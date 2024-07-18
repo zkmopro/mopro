d2e36302ef9774310a1e30bbe329514e5c65470c
Artem <48178500+ElusAegis@users.noreply.github.com>
2024-07-18 16:46:51 +0100

    (feat: android) Added a sample of using Halo2 in Android, as well as improved the UI to visually switch between Circom and Halo2 (#218)

--- test-e2e/android/app/src/main/java/com/mopro/mopro_app/FibonacciComponent.kt
+++ test-e2e/android/app/src/main/java/com/mopro/mopro_app/FibonacciComponent.kt
@@ -1,72 +1,76 @@
+package com.mopro.mopro_app
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.mopro.mopro_app.getFilePathFromAssets
 import uniffi.mopro.GenerateProofResult
-import uniffi.mopro.generateCircomProof
-import uniffi.mopro.verifyCircomProof
+import uniffi.mopro.generateHalo2Proof
+import uniffi.mopro.verifyHalo2Proof
 
 @Composable
-fun MultiplierComponent() {
-    var initTime by remember { mutableStateOf("init time:") }
+fun FibonacciComponent() {
     var provingTime by remember { mutableStateOf("proving time:") }
     var verifyingTime by remember { mutableStateOf("verifying time: ") }
     var valid by remember { mutableStateOf("valid:") }
-    var output by remember { mutableStateOf("output:") }
     var res by remember {
         mutableStateOf<GenerateProofResult>(
             GenerateProofResult(proof = ByteArray(size = 0), inputs = ByteArray(size = 0))
         )
     }
 
-    val inputs = mutableMapOf<String, List<String>>()
-    inputs["a"] = listOf("3")
-    inputs["b"] = listOf("5")
+    val srsPath = getFilePathFromAssets("fibonacci_srs.bin")
+    val provingKeyPath = getFilePathFromAssets("fibonacci_pk.bin")
+    val verifyingKeyPath = getFilePathFromAssets("fibonacci_vk.bin")
 
-    val zkeyPath = getFilePathFromAssets("multiplier2_final.zkey")
+
+    val inputs = mutableMapOf<String, List<String>>()
+    inputs["out"] = listOf("55")
 
     Box(modifier = Modifier.fillMaxSize().padding(16.dp), contentAlignment = Alignment.Center) {
         Button(
             onClick = {
-                Thread(
-                    Runnable {
-                        val startTime = System.currentTimeMillis()
-                        res = generateCircomProof(zkeyPath, inputs)
-                        val endTime = System.currentTimeMillis()
-                        provingTime = "proving time: " + (endTime - startTime).toString() + " ms"
-                    }
-                ).start()
+                Thread {
+                    val startTime = System.currentTimeMillis()
+                    res = generateHalo2Proof(srsPath, provingKeyPath, inputs)
+                    val endTime = System.currentTimeMillis()
+                    provingTime =
+                        "proving time: " +
+                                (endTime - startTime).toString() +
+                                " ms"
+                }
+                    .start()
             },
             modifier = Modifier.padding(top = 20.dp)
         ) { Text(text = "generate proof") }
         Button(
             onClick = {
                 val startTime = System.currentTimeMillis()
-                valid = "valid: " + verifyCircomProof(zkeyPath, res.proof, res.inputs).toString()
+                valid = "valid: " + verifyHalo2Proof(srsPath, verifyingKeyPath, res.proof, res.inputs).toString()
                 val endTime = System.currentTimeMillis()
                 verifyingTime = "verifying time: " + (endTime - startTime).toString() + " ms"
-                output = "output: " + uniffi.mopro.toEthereumInputs(res.inputs)
             },
             modifier = Modifier.padding(top = 120.dp)
         ) { Text(text = "verify proof") }
         Text(
-            text = "Multiplier proof",
+            text = "Halo2 Fibonacci proof",
             modifier = Modifier.padding(bottom = 180.dp),
             fontWeight = FontWeight.Bold
         )
 
-        Text(text = initTime, modifier = Modifier.padding(top = 200.dp).width(200.dp))
         Text(text = provingTime, modifier = Modifier.padding(top = 250.dp).width(200.dp))
         Text(text = valid, modifier = Modifier.padding(top = 300.dp).width(200.dp))
         Text(text = verifyingTime, modifier = Modifier.padding(top = 350.dp).width(200.dp))
-        Text(text = output, modifier = Modifier.padding(top = 400.dp).width(200.dp))
     }
 }
