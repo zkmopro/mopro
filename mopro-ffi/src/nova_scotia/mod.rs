7c46b2016220444565a3b6554049fa5793428b87
csiejimmyliu <91661606+csiejimmyliu@users.noreply.github.com>
2024-09-17 01:24:15 +0800

    test pass with bn256_grumpkin, compress done

--- mopro-ffi/src/nova_scotia/mod.rs
+++ mopro-ffi/src/nova_scotia/mod.rs
@@ -7,7 +7,7 @@ use nova_scotia::*;
 use nova_snark::{
     provider,
     traits::{circuit::TrivialTestCircuit, Group},
-    CompressedSNARK, PublicParams, RecursiveSNARK,
+    CompressedSNARK, PublicParams, RecursiveSNARK, ProverKey, VerifierKey,
 };
 use pasta_curves;
 use pasta_curves::{Fp, Fq};
@@ -45,7 +45,7 @@ macro_rules! nova_scotia_app {
 
         // Return value should be Result<bool, mopro_ffi::MoproError>
         fn verify_nova_scotia_proof(
-            recursive_snark: RecursiveSNARK<G1, G2, C1<G1>, C2<G2>>,
+            recursive_snark: &RecursiveSNARK<G1, G2, C1<G1>, C2<G2>>,
             pp: &PublicParams<G1, G2, C1<G1>, C2<G2>>,
             iteration_count: usize,
             start_public_input: [F<G1>; 2],
@@ -63,6 +63,44 @@ macro_rules! nova_scotia_app {
                 mopro_ffi::MoproError::NovaScotiaError(format!("error verifying proof: {}", e))
             })
         }
+
+        // Compress SNARK
+        fn compress_snark_proof(
+            recursive_snark: RecursiveSNARK<G1, G2, C1<G1>, C2<G2>>,
+            pp: &PublicParams<G1, G2, C1<G1>, C2<G2>>,
+            pk: &ProverKey<G1, G2, C1<G1>, C2<G2>, S<G1>, S<G2>>,
+        ) -> Result<CompressedSNARK<G1, G2, C1<G1>, C2<G2>, S<G1>, S<G2>>, mopro_ffi::MoproError> {
+            
+            
+            let res = CompressedSNARK::<_, _, _, _, S<G1>, S<G2>>::prove(&pp, &pk, &recursive_snark);
+
+            // assert!(res.is_ok());
+            res.map_err(|e| {
+                mopro_ffi::MoproError::NovaScotiaError(format!("error compress proof: {}", e))
+            })
+        }
+
+        fn verify_compressed_proof(
+            compressed_snark: CompressedSNARK<G1, G2, C1<G1>, C2<G2>,S<G1>, S<G2>>,
+            vk: &VerifierKey<G1, G2, C1<G1>, C2<G2>, S<G1>, S<G2>>,
+            iteration_count: usize,
+            start_public_input: [F<G1>; 2],
+            z0_secondary: [F<G2>; 1],
+        ) -> Result<(Vec<F<G1>>, Vec<F<G2>>), mopro_ffi::MoproError> {
+            
+            let res = compressed_snark.verify(
+                &vk,
+                iteration_count,
+                start_public_input.clone().to_vec(),
+                z0_secondary.to_vec(),
+            );
+
+            // assert!(res.is_ok());
+            res.map_err(|e| {
+                mopro_ffi::MoproError::NovaScotiaError(format!("error verifying proof: {}", e))
+            })
+        }
+        
         
     };
 }
@@ -80,7 +118,7 @@ mod test {
     use nova_snark::{
         provider,
         traits::{circuit::TrivialTestCircuit, Group},
-        CompressedSNARK, PublicParams, RecursiveSNARK,
+        CompressedSNARK, PublicParams, RecursiveSNARK, ProverKey, VerifierKey,
     };
 
     use crate as mopro_ffi;
@@ -91,8 +129,8 @@ mod test {
 
     nova_scotia_app!();
 
-    const R1CS_PATH: &str = "../test-vectors/nova_scotia/main.r1cs";
-    const WASM_PATH: &str = "../test-vectors/nova_scotia/toy_js/main.wasm";
+    const R1CS_PATH: &str = "../test-vectors/nova_scotia/toy.r1cs";
+    const WASM_PATH: &str = "../test-vectors/nova_scotia/toy_js/toy.wasm";
 
     #[test]
     fn test_generate_and_verify_nova_scotia_proof() {
@@ -123,7 +161,7 @@ mod test {
         // create public parameters(CRS)
         let pp = create_public_params::<G1, G2>(r1cs.clone());
         let z0_secondary = [F::<G2>::from(0)];
-
+        
         if let Ok(proof_result) = generate_nova_scotia_proof(
             witness_generator_file,
             r1cs,
@@ -133,18 +171,81 @@ mod test {
         ) {
             
             let result = verify_nova_scotia_proof(
-                proof_result,
+                &proof_result,
                 &pp,
                 iteration_count,
                 start_public_input,
                 z0_secondary,
             );
             
-            assert!(result.is_ok());
+            //assert!(result.is_ok());
+            let (pk, vk) = CompressedSNARK::<_, _, _, _, S<G1>, S<G2>>::setup(&pp).unwrap();
+
+            if let Ok(compressed_proof_result) = compress_snark_proof(
+                proof_result,
+                &pp,
+                &pk,
+            ) {
+                
+                let result = verify_compressed_proof(
+                    compressed_proof_result,
+                    &vk,
+                    iteration_count,
+                    start_public_input,
+                    z0_secondary,
+                );
+                
+                assert!(result.is_ok());
+
+            } else {
+                panic!("Failed to generate the compressed proof!")
+            }
 
         } else {
             panic!("Failed to generate the proof!")
         }
 
+
+        
+
+
+        
+
+        /* 
+        let mut recursive_snark = create_recursive_circuit(
+            FileLocation::PathBuf(witness_generator_file.clone()),
+            r1cs.clone(),
+            private_inputs,
+            start_public_input.to_vec(),
+            &pp,
+        ).unwrap();
+
+        println!("Verifying a RecursiveSNARK...");
+        let res = recursive_snark.verify(&pp, iteration_count, &start_public_input, &z0_secondary);
+
+        assert!(res.is_ok());
+        let z_last = res.unwrap().0;
+
+        println!("Generating a CompressedSNARK using Spartan with IPA-PC...");
+        let (pk, vk) = CompressedSNARK::<_, _, _, _, S<G1>, S<G2>>::setup(&pp).unwrap();
+        let res = CompressedSNARK::<_, _, _, _, S<G1>, S<G2>>::prove(&pp, &pk, &recursive_snark);
+        assert!(res.is_ok());
+
+
+        let compressed_snark = res.unwrap();
+
+        // verify the compressed SNARK
+        println!("Verifying a CompressedSNARK...");
+
+        let res = compressed_snark.verify(
+            &vk,
+            iteration_count,
+            start_public_input.to_vec(),
+            z0_secondary.to_vec(),
+        );
+        assert!(res.is_ok());
+        */
+
+        
     }
 }
