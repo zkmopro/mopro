980be53551774b7e1f9b2cc9bf5e5c8d39f17921
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-10-22 18:10:41 +0800

    chore: add ios, android e2e tests

--- test-e2e/tests/ashlang.rs
+++ test-e2e/tests/ashlang.rs
@@ -1,1 +1,4 @@
-uniffi::build_foreign_language_testcases!("tests/bindings/test_ashlang_example.swift",);
+uniffi::build_foreign_language_testcases!(
+    "tests/bindings/test_ashlang_example.swift",
+    "tests/bindings/test_ashlang_example.kts",
+);
