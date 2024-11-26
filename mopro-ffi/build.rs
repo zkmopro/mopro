683c8e75858159f0e25a99873150cf56707a5e8d
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-11-26 17:54:51 +0800

    fix: fix dependencies

--- mopro-ffi/build.rs
+++ mopro-ffi/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    #[cfg(feature = "circom")]
     if std::env::var("MOPRO_FFI_LINK_TEST_WITNESS").unwrap_or_default() != "" {
         rust_witness::transpile::transpile_wasm("../test-vectors/circom".to_string());
     }
