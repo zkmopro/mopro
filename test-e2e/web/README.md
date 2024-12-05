5b1c721e513067705b7881cbc4f866cb20888f22
JinHwan <sifnoc@users.noreply.github.com>
2024-12-05 20:21:41 +0900

    Mopro wasm halo2 (#254)
    * feat: added wasm exporter for halo2
    
    * chore: added gh action for testing halo2 wasm
    
    * fix: lint
    
    * fix: repo updates with Cargo
    
    * feat: added simple web page for testing halo2 wasm code
    
    * feat: added html style, test script with selenium driver
    
    * feat: add test steps for halo2-wasm-web in gh action workflow
    
    * fix: gh action workflow
    
    * fix: halo2 wasm app workflow
    
    * chore: updated documentation and readme
    
    * Update dependency and compiled wasm circuits
    
    * refactor: applied suggestions in PR review
    
    * fix: using multi-thread on wasm
    
    * chore: update compiled wasm
    
    * fix: wasm proof & verify functions
    
    * fix: initialize wasm instance in test_halo2.js
    
    * fix: test cases in workflow and local
    
    * fix: check test case validity while testing on halo2 wasm
    
    * fix: use 'lite-server' instead of 'serve' for serving files outside of root
    
    * updated docs and names following PR reviews
    
    * refactor: test web page and testing script for expliciting web-worker on dev tool

---
+++ test-e2e/web/README.md
@@ -0,0 +1,46 @@
+# Simple Web App with Wasm
+
+## Preparations
+
+Follow these steps only if there are changes in the `mopro-wasm` package. Otherwise, these steps are not required.
+
+### Compile Halo2 Circuits to Wasm
+
+This web test page requires three Wasm circuits: `halo2-plonk-fibonacci`, `halo2-hyperplonk-fibonacci`, and `halo2-gemini-fibonacci`.  
+Refer to [`mopro-wasm/README.md`](../../mopro-wasm/README.md) for more details.
+
+Use `wasm-pack` to generate mopro wasm package for this testing web app with the following commands:
+
+```bash
+mopro-wasm $ wasm-pack build --target web --out-dir ../test-e2e/web/mopro-pkg  -- --all-features
+```
+
+## Test
+
+This test suite uses chromedriver to run the application in a browser in headless mode. Both chrome and chromedriver are required.
+Refer to the [mopro-wasm - Getting Start](../../mopro-wasm/README.md#getting-started) for installation and setup instructions.
+
+1. Install dependencies
+
+    ```bash
+    yarn
+    ```
+
+2. Start a simple server by running the following command:
+
+    ```bash
+    yarn start
+    ```
+
+3. Test the application either manually or by using the test script:
+
+   **Manually**:
+   - Open your browser and navigate to: [http://localhost:3000](http://localhost:3000)
+      
+      ![Result of test on web browser](manual-test-result.png)
+
+   **Using the test script**:
+   - Run the following command:
+     ```bash
+     yarn test
+     ```
---
+++ test-e2e/web/index.js
@@ -0,0 +1,55 @@
+const { Builder, By } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+
+(async function testStatusCheck() {
+    // Configure chrome if env set
+    const options = new chrome.Options();
+    if (process.env.CHROME_BIN) {
+        options.setChromeBinaryPath(process.env.CHROME_BIN);
+    }
+    options.addArguments('--headless');
+    options.addArguments('--no-sandbox');
+
+    const driverBuilder = new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options);
+    
+    // Configure chromewdriver if env set
+    if (process.env.CHROMEDRIVER_BIN) {
+        const service = new chrome.ServiceBuilder(process.env.CHROMEDRIVER_BIN);
+        driverBuilder.setChromeService(service);
+    }
+    
+    try {
+        const driver = await driverBuilder.build();
+        
+        // Log ChromeDriver version via WebDriver
+        const driverVersion = await driver.executeScript('return navigator.userAgent');
+        console.log(`WebDriver user agent:\n${driverVersion}`);
+
+        await driver.get('http://localhost:3000');
+
+        // Wait for the test completion marker
+        const statusDiv = await driver.findElement(By.id('test-status'));
+        await driver.wait(async () => {
+            const status = await statusDiv.getAttribute('data-status');
+            return status === 'passed' || status === 'failed';
+        }, 10000);
+
+        // Check the final test status
+        const finalStatus = await statusDiv.getAttribute('data-status');
+
+        if (finalStatus === 'passed') {
+            console.log("All tests passed!");
+            process.exit(0);
+        } else {
+            console.log("Some test(s) failed");
+            process.exit(1);
+        }
+    } catch (error) {
+        console.error("Error during test:", error);
+        process.exit(1);
+    } finally {
+        await driver.quit();
+    }
+})();
