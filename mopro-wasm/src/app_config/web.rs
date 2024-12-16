342c5a61de6a73eca6d8b4f1cd9e7b7723a5cc39
sifnoc <sifnoc09@gmail.com>
2024-12-16 14:03:00 +0000

    fix(cli): fixing build script on mopro-cli

--- mopro-wasm/src/app_config/web.rs
+++ mopro-wasm/src/app_config/web.rs
@@ -1,13 +1,21 @@
-use std::{path::Path, process::Command};
+use std::fs;
+use std::path::Path;
+use std::process::Command;
 
 pub fn build() {
     // The `mopro-wasm` package requires compilation using the nightly version of Rust.
     // The build method that uses the `wasm-pack` crate cannot be called directly in `test-e2e` as it relies on stable Rust.
     // Rust does not support mixing different toolchains or targets in a single build, hence this separation.
     // Note: There is a known another issue with `wasm-pack` that may affect the build process. For more information, see: https://github.com/rustwasm/wasm-pack/issues/1400.
-
     let cwd = std::env::current_dir().expect("Failed to get current directory");
     let bindings_dest = Path::new(&cwd).join("MoproWasmBindings");
+
+    // Check if `wasm-pack` command is available
+    let check_status = Command::new("wasm-pack").arg("--version").status();
+    if check_status.is_err() || !check_status.unwrap().success() {
+        eprintln!("Error: `wasm-pack` command not found. Please install `wasm-pack` to proceed.");
+        std::process::exit(1);
+    }
 
     // Search the `mopro-wasm` directory
     let mopro_wasm_dir = if cwd
@@ -19,17 +27,10 @@ pub fn build() {
             .expect("Failed to get parent directory")
             .join("mopro-wasm")
     } else {
-        // When running the script from the CLI template
+        // When running the script from the CLI template or else, download 'mopro-wasm' from repo
+        fetch_mopro_wasm();
         cwd.join("mopro-wasm")
     };
-
-    // Check if `wasm-pack` command is available
-    let check_status = Command::new("wasm-pack").arg("--version").status();
-
-    if check_status.is_err() || !check_status.unwrap().success() {
-        eprintln!("Error: `wasm-pack` command not found. Please install `wasm-pack` to proceed.");
-        std::process::exit(1);
-    }
 
     let output = Command::new("rustup")
         .current_dir(mopro_wasm_dir)
@@ -54,5 +55,61 @@ pub fn build() {
         eprintln!("Error details:\n{}", stderr_str);
         eprintln!("mopro-wasm package build failed.");
         std::process::exit(1);
+    }
+
+    // fetches and extract the `mopro-wasm` crate from mopro.
+    fn fetch_mopro_wasm() {
+        let cwd = std::env::current_dir().expect("Failed to get current directory");
+
+        // This should be align the `mopro-wasm` dependency on `cli/Cargo.toml`
+        let repo_url = "https://github.com/sifnoc/mopro";
+        let branch = "mopro-cli-web";
+        let repo_dir = cwd.join("mopro");
+        let target_dir = "mopro-wasm";
+
+        // Fetch without checkout for avoid download all
+        let status = Command::new("git")
+            .args(["clone", "--no-checkout", repo_url, "--branch", branch])
+            .status()
+            .expect("Failed to execute git clone");
+        if !status.success() {
+            panic!("Failed to clone the repository.");
+        }
+
+        let sparse_checkout = Command::new("git")
+            .current_dir(repo_dir.clone())
+            .args(["sparse-checkout", "init", "--cone"])
+            .status()
+            .expect("Failed to initialize sparse-checkout");
+        if !sparse_checkout.success() {
+            panic!("Failed to initialize sparse-checkout.");
+        }
+
+        let set_sparse_checkout = Command::new("git")
+            .current_dir(repo_dir.clone())
+            .args(["sparse-checkout", "set", target_dir])
+            .status()
+            .expect("Failed to set sparse-checkout path");
+        if !set_sparse_checkout.success() {
+            panic!("Failed to set sparse-checkout path.");
+        }
+
+        // Actual fetch file on 'mopro-wasm' in here
+        let checkout = Command::new("git")
+            .current_dir(repo_dir.clone())
+            .args(["checkout"])
+            .status()
+            .expect("Failed to checkout the files on 'mopro-wasm'");
+        if !checkout.success() {
+            panic!("Failed to checkout the files on 'mopro-wasm.");
+        }
+
+        // Move from 'mopro/mopro-wasm' to 'mopro-wasm'
+        let source_path = repo_dir.join(target_dir);
+        let destination_path = cwd.join(target_dir);
+        fs::rename(&source_path, &destination_path)
+            .expect("Failed to move mopro-wasm directory to the root");
+        let _ = fs::remove_dir_all(&repo_dir);
+        println!("Fetch 'mopro-wasm' sucessfully")
     }
 }
