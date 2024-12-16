b112a99b666267836af0eb9e2801aa6cadec662f
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-12-16 15:44:23 +0800

    chore: remove unused changes

--- test-e2e/src/bin/ios.rs
+++ test-e2e/src/bin/ios.rs
@@ -1,5 +1,3 @@
 fn main() {
-    // A simple wrapper around a build command provided by mopro.
-    // In the future this will likely be published in the mopro crate itself.
     mopro_ffi::app_config::ios::build();
 }
