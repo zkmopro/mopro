1bcf79531020f33c0f76584943834c2d7abcc2d3
vimwitch <jchancehud@gmail.com>
2025-01-09 03:54:00 -0800

    fix: dynamically look for c++ stdlib path based on compiler

--- mopro-ffi/build.rs
+++ mopro-ffi/build.rs
@@ -13,10 +13,17 @@ fn main() {
     let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
     let absolute_lib_path = manifest_dir.join("rapidsnark").join(arch);
 
+    let compiler = cc::Build::new().get_compiler();
+    let cpp_stdlib = if compiler.is_like_clang() {
+        "c++"
+    } else {
+        "stdc++"
+    };
+
     println!("cargo:rustc-link-search=native={}", absolute_lib_path.clone().display());
 
     println!("cargo:rustc-link-lib=static=rapidsnark");
-    println!("cargo:rustc-link-lib=c++");
+    println!("cargo:rustc-link-lib={cpp_stdlib}");
     println!("cargo:rustc-link-lib=pthread");
     println!("cargo:rustc-link-lib=fr");
     println!("cargo:rustc-link-lib=fq");
