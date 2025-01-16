e459f1a5ddf2bee6c6130cf5e014100d5505502b
vimwitch <jchancehud@gmail.com>
2025-01-16 01:43:41 -0800

    chore: proper static libraries

--- mopro-ffi/build.rs
+++ mopro-ffi/build.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use std::fs;
 
 fn main() {
     #[cfg(feature = "circom")]
@@ -11,7 +12,12 @@ fn main() {
 
     // Try to list contents of the target directory
     let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
-    let absolute_lib_path = manifest_dir.join("rapidsnark").join(arch);
+    let rapidsnark_dir = manifest_dir.join("rapidsnark");
+    let absolute_lib_path = if fs::exists(rapidsnark_dir.join(&target)).unwrap() {
+        rapidsnark_dir.join(target)
+    } else {
+        rapidsnark_dir.join(arch)
+    };
 
     let compiler = cc::Build::new().get_compiler();
     let cpp_stdlib = if compiler.is_like_clang() {
