cf4e1e259a3df42ff168702ac58869f72d4c0f69
vimwitch <jchancehud@gmail.com>
2025-01-29 09:13:41 -0800

    refactor: remove ashlang

--- test-e2e/tests/circom.rs
+++
@@ -1,8 +0,0 @@
-uniffi::build_foreign_language_testcases!(
-    "tests/bindings/test_circom_multiplier2.swift",
-    "tests/bindings/test_circom_multiplier2.kts",
-    "tests/bindings/test_circom_keccak.swift",
-    "tests/bindings/test_circom_keccak.kts",
-    "tests/bindings/test_circom_multiplier2_bls.swift",
-    "tests/bindings/test_circom_multiplier2_bls.kts",
-);
