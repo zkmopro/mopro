1375236f6c4b4e778f013e9e7966b0c265ccbcc9
vimwitch <jchancehud@gmail.com>
2025-01-29 09:18:41 -0800

    fix: re-add e2e tests

---
+++ test-e2e/tests/halo2.rs
@@ -0,0 +1,10 @@
+uniffi::build_foreign_language_testcases!(
+    "tests/bindings/test_plonk_fibonacci.swift",
+    "tests/bindings/test_plonk_fibonacci.kts",
+    "tests/bindings/test_halo2_keccak256.swift",
+    "tests/bindings/test_halo2_keccak256.kts",
+    "tests/bindings/test_hyperplonk_fibonacci.swift",
+    "tests/bindings/test_hyperplonk_fibonacci.kts",
+    "tests/bindings/test_gemini_fibonacci.swift",
+    "tests/bindings/test_gemini_fibonacci.kts"
+);
