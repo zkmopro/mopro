e264e17219d8980e1e3ef0584610de6c59ec2cae
KimiWu <leonhartwu@gmail.com>
2025-02-21 09:57:49 +0800

    fix witness fn type in circom template

--- cli/src/template/circom/lib.rs
+++ cli/src/template/circom/lib.rs
@@ -2,5 +2,5 @@
 rust_witness::witness!(multiplier2);
 
 mopro_ffi::set_circom_circuits! {
-    ("multiplier2_final.zkey", multiplier2_witness)
+    ("multiplier2_final.zkey", WitnessFn::RustWitness(multiplier2_witness))
 }
