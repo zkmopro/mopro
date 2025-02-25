4418e5e7455beefc43ae90b222a3b71a95acff9b
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2025-02-25 20:57:49 +0800

    fix: fix build

--- circom-prover/build.rs
+++ circom-prover/build.rs
@@ -1,6 +1,9 @@
 fn main() {
-    #[cfg(feature = "rustwitness")]
-    rust_witness::transpile::transpile_wasm("./test-vectors".to_string());
-    #[cfg(feature = "witnesscalc")]
-    witnesscalc_adapter::build_and_link("./test-vectors");
+    let crate_name = std::env::var("CARGO_PKG_NAME").unwrap();
+    if crate_name == "circom-prover" {
+        #[cfg(feature = "rustwitness")]
+        rust_witness::transpile::transpile_wasm("./test-vectors".to_string());
+        #[cfg(feature = "witnesscalc")]
+        witnesscalc_adapter::build_and_link("./test-vectors");
+    }
 }
