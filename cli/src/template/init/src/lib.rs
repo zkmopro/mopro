a550c4d196e3d54d8461fbde5993db107604fe49
sifnoc <sifnoc09@gmail.com>
2025-02-28 15:56:00 +0900

    fix: update templates in cli for updating uniffi proc-macro

--- cli/src/template/init/src/lib.rs
+++ cli/src/template/init/src/lib.rs
@@ -1,5 +1,7 @@
-// Here we're calling a macro exported by uniffi. This macro will
-// write some functions and bind them to the uniffi UDL file. These
+use circom_prover::witness::WitnessFn;
+
+// Here we're calling a macro exported with Uniffi. This macro will
+// write some functions and bind them to FFI type. These
 // functions will invoke the `get_circom_wtns_fn` generated below.
 mopro_ffi::app!();
 
