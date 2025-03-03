51a272eea9a4e66558361830d6b239b107ab5ce0
KimiWu <leonhartwu@gmail.com>
2025-03-03 16:41:34 +0800

    using witnesscalc_adapter::build_and_link

--- mopro-ffi/build.rs
+++ mopro-ffi/build.rs
@@ -8,26 +8,7 @@ fn main() {
 
         #[cfg(feature = "circom-wit-witnesscalc")]
         {
-            // witnesscalc only: copy .dat files to the target folder
-            // TODO: usning `witnesscalc_adapter::build_and_link()`: https://github.com/zkmopro/mopro/issues/345
-            let out_dir = std::env::var("OUT_DIR").expect("Failed to get OUT_DIR");
-            let target_dir = std::path::PathBuf::from(out_dir).join("witnesscalc/src");
-            std::fs::create_dir_all(&target_dir).unwrap();
-
-            for entry in std::fs::read_dir("../test-vectors/circom/witnesscalc")
-                .expect("Failed to read source directory")
-            {
-                let entry = entry.expect("Failed to get directory entry");
-                let src_path = entry.path();
-                let dest_path = target_dir.join(entry.file_name());
-
-                if let Err(e) = std::fs::copy(&src_path, &dest_path) {
-                    eprintln!(
-                        "Failed to copy file {:?} to {:?}: {}",
-                        src_path, dest_path, e
-                    );
-                }
-            }
+            witnesscalc_adapter::build_and_link("../test-vectors/circom/witnesscalc");
         }
     }
 }
