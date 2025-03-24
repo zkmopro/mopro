f00db7c938fde99a4ce01244a7d99bb5968b4928
KimiWu <leonhartwu@gmail.com>
2025-03-24 11:43:42 +0800

    refine verify_circom_proof in rapidsnark

--- circom-prover/src/prover/rapidsnark.rs
+++ circom-prover/src/prover/rapidsnark.rs
@@ -1,24 +1,17 @@
+use super::circom::{CURVE_BLS12_381, CURVE_BN254};
+use super::{ark_circom::read_proving_key, serialization::SerializableInputs, PublicInputs};
+use crate::prover::circom::PROTOCOL_GROTH16;
 use crate::CircomProof;
-use anyhow::Result;
-use ark_bn254::{Bn254, Fr};
+use anyhow::{bail, Result};
+use ark_bn254::{Bn254, Fq, Fq2, Fr};
 use ark_ec::pairing::Pairing;
 use ark_ff::PrimeField;
-use ark_groth16::{prepare_verifying_key, Proof};
+use ark_groth16::{prepare_verifying_key, PreparedVerifyingKey, Proof};
 use num::BigUint;
 use num_bigint::BigInt;
-use serde_json::json;
+use serde_json::{json, Value};
 use std::thread::JoinHandle;
 use std::{fs::File, str::FromStr};
-
-use super::serialization::SerializableProof;
-use super::{
-    ark_circom::read_proving_key,
-    serialization::{self, SerializableInputs},
-    PublicInputs,
-};
-
-type Fq = ark_bn254::Fq;
-type Fq2 = ark_bn254::Fq2;
 
 pub fn generate_circom_proof(
     zkey_path: String,
@@ -43,7 +36,6 @@ pub fn generate_circom_proof(
     let c = <Bn254 as Pairing>::G1Affine::new_unchecked(c_x, c_y);
     let b1_x = Fq::from_str(proof_json["pi_b"][0][0].as_str().unwrap()).unwrap();
     let b1_y = Fq::from_str(proof_json["pi_b"][0][1].as_str().unwrap()).unwrap();
-    // let b1 = <Bn254 as Pairing>::G2Affine::new_unchecked(b1_x, b1_y);
     let b1 = Fq2::new(b1_x, b1_y);
     let b2_x = Fq::from_str(proof_json["pi_b"][1][0].as_str().unwrap()).unwrap();
     let b2_y = Fq::from_str(proof_json["pi_b"][1][1].as_str().unwrap()).unwrap();
@@ -65,41 +57,45 @@ pub fn generate_circom_proof(
     })
 }
 
+// Only support Bn254 (Rapidsnark doesn't support BLS12-381 yet)
 pub fn verify_circom_proof(zkey_path: String, proof: CircomProof) -> Result<bool> {
-    let serialized_proof =
-        serialization::serialize_proof::<Bn254>(&SerializableProof(proof.proof.into()));
-    let proof_parsed = serialization::deserialize_proof::<Bn254>(serialized_proof);
-    let public_inputs_parsed: SerializableInputs<Bn254> = proof.pub_inputs.into();
+    if proof.proof.curve.eq(CURVE_BLS12_381) {
+        bail!("Not support {} yet", CURVE_BLS12_381)
+    }
 
+    let public_inputs_parsed: SerializableInputs<Bn254> = proof.pub_inputs.into();
     let pi_a: Vec<String> = vec![
-        proof_parsed.0.a.x.to_string(),
-        proof_parsed.0.a.y.to_string(),
-        "1".to_string(),
+        proof.proof.a.x.to_string(),
+        proof.proof.a.y.to_string(),
+        proof.proof.a.z.to_string(),
     ];
 
     let pi_b: Vec<Vec<String>> = vec![
         vec![
-            proof_parsed.0.b.x.c0.to_string(),
-            proof_parsed.0.b.x.c1.to_string(),
+            proof.proof.b.x[0].to_string(),
+            proof.proof.b.x[1].to_string(),
         ],
         vec![
-            proof_parsed.0.b.y.c0.to_string(),
-            proof_parsed.0.b.y.c1.to_string(),
+            proof.proof.b.y[0].to_string(),
+            proof.proof.b.y[1].to_string(),
         ],
-        vec!["1".to_string(), "0".to_string()],
+        vec![
+            proof.proof.b.z[0].to_string(),
+            proof.proof.b.z[1].to_string(),
+        ],
     ];
 
     let pi_c: Vec<String> = vec![
-        proof_parsed.0.c.x.to_string(),
-        proof_parsed.0.c.y.to_string(),
-        "1".to_string(),
+        proof.proof.c.x.to_string(),
+        proof.proof.c.y.to_string(),
+        proof.proof.c.z.to_string(),
     ];
 
     let proof_json = json!({
         "pi_a": pi_a,
         "pi_c": pi_c,
         "pi_b": pi_b,
-        "protocol": "groth16",
+        "protocol": PROTOCOL_GROTH16,
     });
 
     let inputs_json = json!(public_inputs_parsed
@@ -119,10 +115,26 @@ pub fn verify_circom_proof(zkey_path: String, proof: CircomProof) -> Result<bool
         .iter()
         .map(|g| vec![g.x.to_string(), g.y.to_string(), "1".to_string()])
         .collect::<Vec<Vec<String>>>();
-    let vkey_json = json!({
-        "protocol": "groth16",
-        "curve": "bn128",
-        "nPublic": public_inputs_parsed.0.len(),
+    let vkey_json = prepare_vkey(pvk, ic, public_inputs_parsed.0.len(), CURVE_BN254);
+    let valid = rust_rapidsnark::groth16_verify_wrapper(
+        &proof_json.to_string(),
+        &inputs_json.to_string(),
+        &vkey_json.to_string(),
+    )?;
+
+    Ok(valid)
+}
+
+fn prepare_vkey(
+    pvk: PreparedVerifyingKey<Bn254>,
+    ic: Vec<Vec<String>>,
+    public_input_len: usize,
+    curve: &str,
+) -> Value {
+    json!({
+        "protocol": PROTOCOL_GROTH16,
+        "curve": curve,
+        "nPublic": public_input_len,
         "vk_alpha_1": vec![
             pvk.vk.alpha_g1.x.to_string(),
             pvk.vk.alpha_g1.y.to_string(),
@@ -192,12 +204,5 @@ pub fn verify_circom_proof(zkey_path: String, proof: CircomProof) -> Result<bool
             ],
         ],
         "IC": ic
-    });
-    let valid = rust_rapidsnark::groth16_verify_wrapper(
-        &proof_json.to_string(),
-        &inputs_json.to_string(),
-        &vkey_json.to_string(),
-    )?;
-
-    Ok(valid)
+    })
 }
