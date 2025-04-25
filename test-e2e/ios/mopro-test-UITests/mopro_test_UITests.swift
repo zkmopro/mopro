0b67bdad33122380e6b156cd74de8faa1335527f
KimiWu <leonhartwu@gmail.com>
2025-04-25 15:31:20 +0800

    test: remove ios e2e test

--- test-e2e/ios/mopro-test-UITests/mopro_test_UITests.swift
+++ test-e2e/ios/mopro-test-UITests/mopro_test_UITests.swift
@@ -55,24 +55,4 @@ final class mopro_test_UITests: XCTestCase {
         elementQuery = app.staticTexts.containing(predicate)
         XCTAssert(elementQuery.count == 1)
     }
-
-    func testNoirProveVerify() throws {
-        throw XCTSkip("Skipping testNoirProveVerify because Noir lib doesn't support simulator yet.")
-
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-        
-        app.buttons["proveNoir"].tap()
-        var predicate = NSPredicate(format: "label CONTAINS[c] %@", "1️⃣")
-        var elementQuery = app.staticTexts.containing(predicate)
-        for element in app.staticTexts.allElementsBoundByIndex {
-            NSLog("Static text: %@", element.label)
-        }
-//        XCTAssert(elementQuery.count == 1)
-        app.buttons["verifyNoir"].tap()
-        predicate = NSPredicate(format: "label CONTAINS[c] %@", "2️⃣")
-        elementQuery = app.staticTexts.containing(predicate)
-        XCTAssert(elementQuery.count == 1)
-    }
 }
