c992af07fcbe9bd7ae71d134ff2bd6b7ed18cd4f
Artem <48178500+ElusAegis@users.noreply.github.com>
2025-09-20 13:16:33 +0100

    refactor: remove unused `serializeOutputs` function from circom test

--- cli/src/template/init/tests/bindings/circom/test_multiplier2.swift
+++ cli/src/template/init/tests/bindings/circom/test_multiplier2.swift
@@ -1,31 +1,6 @@
 import Foundation
 import mopro
 
-func serializeOutputs(_ stringArray: [String]) -> [UInt8] {
-    var bytesArray: [UInt8] = []
-    let length = stringArray.count
-    var littleEndianLength = length.littleEndian
-    let targetLength = 32
-    withUnsafeBytes(of: &littleEndianLength) {
-        bytesArray.append(contentsOf: $0)
-    }
-    for value in stringArray {
-        // TODO: should handle 254-bit input
-        var littleEndian = Int32(value)!.littleEndian
-        var byteLength = 0
-        withUnsafeBytes(of: &littleEndian) {
-            bytesArray.append(contentsOf: $0)
-            byteLength = byteLength + $0.count
-        }
-        if byteLength < targetLength {
-            let paddingCount = targetLength - byteLength
-            let paddingArray = [UInt8](repeating: 0, count: paddingCount)
-            bytesArray.append(contentsOf: paddingArray)
-        }
-    }
-    return bytesArray
-}
-
 do {
     let zkeyPath = "../../../test-vectors/circom/multiplier2_final.zkey"
 
