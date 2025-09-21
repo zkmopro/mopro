305b4989b6dfb0b558a55317cd13d255c73a33a7
Artem <48178500+ElusAegis@users.noreply.github.com>
2025-09-21 18:15:17 +0100

    refactor: clean up mode selection logic in `mode_selector`

--- cli/src/build/mode_selector.rs
+++ cli/src/build/mode_selector.rs
@@ -6,21 +6,21 @@ use mopro_ffi::app_config::constants::Mode;
 pub(super) fn select_mode(config: &mut Config) -> anyhow::Result<Mode> {
     let theme = ColorfulTheme::default();
     let mut selection = Select::with_theme(&theme);
+    let options = Mode::all_strings();
 
-    // Get default based on previous selection.
-    if let Some(build_mode) = config.build_mode.clone() {
-        if let Some(idx) = Mode::idx(build_mode.as_ref()) {
+    if let Some(build_mode) = config.build_mode.as_ref() {
+        if let Some(idx) = Mode::idx(build_mode.as_str()) {
             selection.default(idx);
         }
-    };
+    }
 
     let idx = selection
         .with_prompt("Build mode")
-        .items(Mode::all_strings().as_ref())
+        .items(options.as_ref())
         .interact()?;
 
-    // update user's selection
-    config.build_mode = Some(Mode::from_idx(idx).as_str().to_string());
+    let mode = Mode::from_idx(idx);
+    config.build_mode = Some(mode.as_str().to_string());
 
-    Ok(Mode::from_idx(idx))
+    Ok(mode)
 }
