fb17f67f699629324c95d38ec2919094b3aad25f
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2025-09-28 12:28:04 +0800

    mopro-cli v0.3.0

--- cli/src/build.rs
+++ cli/src/build.rs
@@ -217,6 +217,34 @@ fn copy_mopro_wasm_lib() -> anyhow::Result<()> {
         copy_embedded_dir(&WASM_TEMPLATE_DIR, &target_dir)?;
     }
 
+    let cargo_toml_path = target_dir.join("Cargo.toml");
+    let cargo_toml = "[package]
+name = \"mopro-wasm-lib\"
+version = \"0.1.0\"
+edition = \"2021\"
+
+[lib]
+crate-type = [\"rlib\", \"cdylib\"]
+
+[dependencies]
+mopro-wasm = { git = \"https://github.com/zkmopro/mopro.git\", features = [
+    \"gemini\",
+    \"hyperplonk\",
+    \"plonk\",
+] }
+
+[target.wasm32-unknown-unknown.dependencies]
+console_error_panic_hook = \"0.1.7\"
+getrandom = { version = \"0.2.15\", features = [\"js\"] }
+serde-wasm-bindgen = \"0.6.5\"
+wasm-bindgen = { version = \"0.2.95\", features = [\"serde-serialize\"] }
+wasm-bindgen-console-logger = \"0.1.1\"
+wasm-bindgen-futures = \"0.4.47\"
+wasm-bindgen-rayon = { version = \"1.2.2\", features = [\"no-bundler\"] }
+wasm-bindgen-test = \"0.3.42\"
+";
+
+    std::fs::write(cargo_toml_path, cargo_toml)?;
     Ok(())
 }
 
