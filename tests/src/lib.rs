cea39d41a1a64219fc9dd614baffa42d07a01aee
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2025-09-29 11:18:28 +0800

    fix: fix clippy and typo

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -16,12 +16,3 @@ pub fn mopro_flutter_hello_world() -> String {
     mopro_hello_world()
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn addition_works() {
-        #[cfg(feature = "uniffi")]
-        mopro_ffi::app_config::ios::build();
-        assert!(true);
-    }
-}
