3bc8ac0e4aecb8959547e255cec3ac5d10f7a63f
Alex Kuzmin <6849426+alxkzmn@users.noreply.github.com>
2025-10-03 10:16:52 +0800

    Serialize Circom proof (#598)

--- circom-prover/src/prover.rs
+++ circom-prover/src/prover.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use circom::Proof;
 use num::BigUint;
+use serde::{Deserialize, Serialize};
 use std::{str::FromStr, thread::JoinHandle};
 
 pub mod ark_circom;
@@ -11,13 +12,52 @@ pub mod arkworks;
 #[cfg(feature = "rapidsnark")]
 pub mod rapidsnark;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct PublicInputs(pub Vec<BigUint>);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct CircomProof {
     pub proof: Proof,
     pub pub_inputs: PublicInputs,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::prover::circom::{Proof, G1, G2};
+    use num::BigUint;
+
+    #[test]
+    fn serde_roundtrip_circom_proof() {
+        let a = G1 {
+            x: BigUint::from(1u32),
+            y: BigUint::from(2u32),
+            z: BigUint::from(1u32),
+        };
+        let b = G2 {
+            x: [BigUint::from(3u32), BigUint::from(4u32)],
+            y: [BigUint::from(5u32), BigUint::from(6u32)],
+            z: [BigUint::from(1u32), BigUint::from(0u32)],
+        };
+        let c = G1 {
+            x: BigUint::from(7u32),
+            y: BigUint::from(8u32),
+            z: BigUint::from(1u32),
+        };
+        let proof = Proof {
+            a,
+            b,
+            c,
+            protocol: "groth16".to_string(),
+            curve: "bn128".to_string(),
+        };
+        let pub_inputs = PublicInputs(vec![BigUint::from(9u32), BigUint::from(10u32)]);
+        let cp = CircomProof { proof, pub_inputs };
+
+        let serialized = serde_json::to_string(&cp).unwrap();
+        let deserialized: CircomProof = serde_json::from_str(&serialized).unwrap();
+        assert_eq!(cp, deserialized);
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
